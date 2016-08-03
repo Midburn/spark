d97cffb49590561b4ab86ec56fdf72d7d3f5a86b
רועי צחור <roy.zahor@gmail.com>
2016-08-04 01:36:59 +0300

    Continued payment flow

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,5 @@
 var security = require('../config/security');
-var mail = require('../config/mail');
+var i18next = require('i18next');
 
 module.exports = function (app) {
 
@@ -8,24 +8,25 @@ module.exports = function (app) {
     });
 
     app.get('/npo_pay_fee', security.protectGet, function (req, res, next) {
-        //TODO temp code & parameters - just for testing
         var request = require('request');
         request.post(
-            'https://testicredit.rivhit.co.il/API/PaymentPageRequest.svc/GetUrl',
+            //'https://testicredit.rivhit.co.il/API/PaymentPageRequest.svc/GetUrl', // TEST
+            'https://icredit.rivhit.co.il/API/PaymentPageRequest.svc/GetUrl',       // PROD
             {
                 json: {
-                    "GroupPrivateToken": "7ddd44b1-fcbf-4f95-baea-5169b6788681",
+                    //"GroupPrivateToken": "7ddd44b1-fcbf-4f95-baea-5169b6788681", // TEST
+                    "GroupPrivateToken": "bf0c4ab6-183d-4a97-9ca0-54df2d933e1e",   // PROD
                     "Items": [{
-                        "Id": 6969,
+                        "Id": 0,
                         "Quantity": 1,
-                        "UnitPrice": 60,
-                        "Description": "Membership Fee 2016"
+                        "UnitPrice": 1,
+                        "Description": i18next.t('membership_fee', {year: 2016})
                     }],
-                    "RedirectURL": "http://localhost:3000/home",
+                    "RedirectURL": "http://localhost:3000/payment_received",
                     "ExemptVAT": true,
                     "MaxPayments": 1,
-                    "CustomerFirstName": req.user.first_name,
-                    "CustomerLastName": req.user.last_name
+                    "CustomerFirstName": req.user.attributes.first_name,
+                    "CustomerLastName": req.user.attributes.last_name
                 }
             },
             function (error, response, body) {
@@ -41,6 +42,13 @@ module.exports = function (app) {
                 }
             }
         );
+    });
+
+    app.get('/payment_received', security.protectGet, function (req, res, next) {
+        var token = req.params('Token');
+        console.log('Received payment token: ' + token);
+
+        res.render('pages/payment_received', {user: req.user});
     });
 };
 
