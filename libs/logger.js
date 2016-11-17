4e0335562fc8e1ef24ccb7310c0df8462b5819e8
Ron Gross <ron.gross@gmail.com>
2016-11-17 23:46:52 +0200

    Add timestamp

--- libs/logger.js
+++ libs/logger.js
@@ -3,21 +3,29 @@ var config = require('config');
 
 module.exports = function(module) {
     var filename = module.id;    
-    
-    function log(level, msg, vars) {
-        winston.log(level, filename + ' : ' + msg, vars); 
-    }
-    
-    return {
-        init : function() {
-            winston.configure({
-                level: 'info'
-                /* , File logger transports: [
-                  new (winston.transports.File)({ filename: 'sparks.log' })
-                ]*/
-            });
-        },
+    var logger = new (winston.Logger)({
+      transports: [
+        new (winston.transports.Console)({
+          timestamp: function() {
+            return Date.now();
+          },
+          formatter: function(options) {
+            // Return string will be passed to logger.
+            return options.timestamp() +' '+ options.level.toUpperCase() +' '+ (options.message ? options.message : '') +
+              (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+          }
+        })
+      ]
+    });
+        
 
+    function log (level, msg, vars) {
+        logger.log(level, filename + ' : ' + msg, vars); 
+    }
+
+            
+    return {
+        log : log,
         
         // Aliases
         debug : function (msg, vars) {
@@ -32,8 +40,5 @@ module.exports = function(module) {
         error  : function (msg, vars) {
             log('error', msg, vars);
         },
-        
-        // Log
-        log : log
-    }
+    };
 }
