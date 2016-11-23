da9224268b7337e337275069f59954ef48cf0e18
רועי צחור <roy.zahor@gmail.com>
2016-11-23 22:07:22 +0200

    Adding dev. tools framework

--- routes/dev_routes.js
+++ routes/dev_routes.js
@@ -1,38 +1,10 @@
-var express = require('express');
-var router = express.Router({mergeParams: true});
-
-var User = require('../models/user').User;
-
-router.get('/', function (req, res) {
-    res.render('dev_tools/dev_home');
-});
-
-router.get('/create-admin', function (req, res) {
-    var newUser = new User({
-        email: 'a',
-        first_name: 'Development',
-        last_name: 'Admin',
-        gender: 'female',
-        validated: true,
-        roles: 'admin'
-    });
-    newUser.generateHash('a');
-
-    newUser.save().then(function (model) {
-            res.redirect("/");
-        }
-    );
-
-    res.redirect("./");
-});
-
-router.get('/view-debug/*', function (req, res) {
+module.exports = function(app) {
+  app.get('/view-debug/*', function(req, res) {
     const path = req.path.substr('/view-debug/'.length);
     if (path === '') {
-        res.render('dev_tools/view-debug');
+      res.render('tools/view-debug');
     } else {
-        res.render('${path}', JSON.parse(req.query.params));
+      res.render(`${path}`, JSON.parse(req.query.params));
     }
-});
-
-module.exports = router;
+  });
+}
