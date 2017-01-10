35902591d325c7d7ca8ea3f3c5b41612940d4f48
Ori Hoch <ori@uumpa.com>
2017-01-10 12:43:27 +0200

    minor comment

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -3,8 +3,10 @@
 # this script assumes "clean" environment like Travis
 # TODO: modify for developer environment which already have the DB
 
+# when PR #3 is merged this will work
 mysql -u root < sql/create_db.sql
 mysql -u root < sql/schema.sql
 mysql -u root < sql/camps.sql
+
 npm install
 npm test
