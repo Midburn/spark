85cb0b720a6ccff652de72052ce931fe4158be06
eladg <ggrelad@gmail.com>
2017-01-22 20:28:32 -0800

    .env support + remove passwords from codebase

--- opsworks.js
+++ opsworks.js
@@ -4,37 +4,53 @@
  * should be modified on production with correct configurations
  */
 
-exports.db = {
-    "client"        : "sqlite3",
-    "debug"         : false,
-    "filename"      : "./dev.sqlite3"
-};
+// reads env config from .env file
+require('dotenv').load();
 
-// mysql configuration example
-// exports.db = {
-//     "client"        : "mysql",
-//     "debug"         : false,
-//     "host"          : "localhost",
-//     "database"      : "spark",
-//     "username"      : "spark",
-//     "password"      : "spark",
-//     "charset"       : "UTF8_GENERAL_CI",
-// };
+switch(process.env.SPARK_DB_TYPE) {
+
+case "mysql":
+    exports.db = {
+        "client"   : process.env.SPARK_DB_CLIENT,
+        "host"     : process.env.SPARK_DB_HOSTNAME,
+        "database" : process.env.SPARK_DB_DBNAME,
+        "username" : process.env.SPARK_DB_USERNAME,
+        "password" : process.env.SPARK_DB_PASSWORD,
+        "charset"  : "UTF8_GENERAL_CI",
+        "debug"    : false,
+    };
+    break;
+
+case "sqlite3":
+    exports.db = {
+        "client"   : process.env.SPARK_DB_CLIENT,
+        "filename" : process.env.SPARK_DB_FILENAME,
+        "debug"    : false,
+    };
+    break;
+
+default:
+    console.log("environment variable SPARK_DB_TYPE is not set or configured wrong.");
+    console.log("See .env-example file for more details.");
+    console.log("");
+    process.exit(1);
+}
 
 exports.server = {
-    port      : 3000,
-    hostname  : "localhost",
-    protocol  : "http",                  // http or https
-    url       : "http://localhost:3000"  // full URL including protocol and port. NO trailing slash
+    port      : process.env.SPARK_SERVER_PORT,
+    hostname  : process.env.SPARK_SERVER_HOSTNAME,
+    protocol  : process.env.SPARK_SERVER_PROTOCOL, // http or https
+    url       : process.env.SPARK_SERVER_URL,      // full URL including protocol and port. NO trailing slash
 };
 
 exports.mail = {
-    enabled         : true,
-    from            : "spark@localhost",
-    host            : "localhost",
-    secureConnection: false,         // use SSL
-    port            : 25,          // port for secure SMTP
-    transportMethod : "SMTP"       // default is SMTP. Accepts anything that nodemailer accepts
+    enabled         : process.env.SPARK_MAILSERVER_ENABLE,
+    from            : process.env.SPARK_MAILSERVER_FROM,
+    host            : process.env.SPARK_MAILSERVER_HOST,
+    port            : process.env.SPARK_MAILSERVER_PORT,   // port for secure SMTP
+    transportMethod : process.env.SPARK_MAILSERVER_METHOD, // default is SMTP. Accepts anything that nodemailer accepts
+    secureConnection: false,                               // use SSL
+
     /*
     auth: {
         user: "",
@@ -44,8 +60,8 @@ exports.mail = {
 };
 
 exports.payment = {
-    // iCreditUrl              : ""
-    // iCreditGroupPrivateToken: ""
+    iCreditUrl              : process.env.SPARK_ICREDIT_URL,
+    iCreditGroupPrivateToken: process.env.SPARK_ICREDIT_PRIVATETOKEN,
 };
 
 exports.npo = {
@@ -54,7 +70,7 @@ exports.npo = {
 };
 
 exports.facebook = {
-    app_id: "1083906121721925",
-    app_secret: "",
-    callbackBase: "http://localhost:3000"
+    app_id: process.env.SPARK_FACEBOOK_APP,
+    app_secret: process.env.SPARK_FACEBOOK_SECRET,
+    callbackBase: process.env.SPARK_FACEBOOK_CALLBACK,
 };
