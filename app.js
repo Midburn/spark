c8ea67cc160852786db048315d6c4a1c61a92c3e
Ori Hoch <ori@uumpa.com>
2017-01-31 13:31:12 +0200

    use gentelella for admin interface

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var log = require('./libs/logger')(module);
 var recaptcha = require('express-recaptcha');
 var compileSass = require('express-compile-sass');
 var recaptchaConfig = require('config').get('recaptcha');
+var KnexSessionStore = require('connect-session-knex')(session);
+var knex = require('./libs/db').knex;
+
 
 log.info('Spark is starting...');
 
@@ -49,6 +52,8 @@ app.use(compileSass({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
 app.use(function(req, res, next) {
     res.locals.req = req;
     res.locals.path = req.path.split('/');
@@ -57,11 +62,13 @@ app.use(function(req, res, next) {
 
 // Passport setup
 require('./libs/passport')(passport);
+var sessionStore = new KnexSessionStore({knex: knex});
 app.use(session({
     secret: 'SparklePoniesAreFlyingOnEsplanade',
     resave: false,
     saveUninitialized: false,
-    maxAge: 1000 * 60 * 30
+    maxAge: 1000 * 60 * 30,
+    store: sessionStore
 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
@@ -139,8 +146,9 @@ app.use(userRole.middleware());
 if (app.get('env') === 'development') {
     app.use('/dev', require('./routes/dev_routes'));
 }
-app.use('/:lng?/admin', require('./routes/admin_routes'));
 require('./routes/main_routes.js')(app, passport);
+
+app.use('/:lng?/admin', require('./routes/admin_routes'));
 
 // Module's Routes
 app.use('/:lng/npo', require('./routes/npo_routes'));
--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -13,14 +13,106 @@ var npoConfig = config.get('npo');
 var serverConfig = config.get('server');
 var log = require('../libs/logger.js')(module);
 
-router.get('/', userRole.isAdmin(), function (req, res, next) {
-    res.render('admin/home.jade', {user: req.user});
+/**
+ * private function to render admin views
+ * creates template params for the views
+ *
+ * currently there are some frontend elements supported in the view but not in backend, you can add them to options if you want:
+ *
+    // options.notifications = {
+    //     number: 8,
+    //     items: [
+    //         {
+    //             title: "John Smith",
+    //             time: "3 mins ago",
+    //             img: {src: "/gentelella/images/user.png", alt: "Profile Image"},
+    //             message: "Film festivals used to be do-or-die moments for movie makers. They were where..."
+    //         },
+    //         {
+    //             title: "John Smith",
+    //             time: "3 mins ago",
+    //             img: {src: "/gentelella/images/user.png", alt: "Profile Image"},
+    //             message: "Film festivals used to be do-or-die moments for movie makers. They were where..."
+    //         }
+    //     ],
+    //     all: {
+    //         text: "See All Alerts",
+    //         href: "/admin/alerts"
+    //     }
+    // };
+
+    options.showUserMenu = true;
+
+     options.sideBarFooter = [
+         {title: "Settings", glyphicon: "cog"},
+         {title: "FullScreen", glyphicon: "fullscreen"},
+         {title: "Lock", glyphicon: "eye-close"},
+         {title: "Logout", glyphicon: "off"}
+     ];
+ *
+ */
+var _adminRender = function(req, res, name, options) {
+    if (!options) options = {};
+    options.user = req.user;
+    options.sideBar = [
+        {title: "Home", icon: "home", href: "/admin"},
+        {title: "Users", icon: "users", href: "/admin/users"},
+        {title: "Camps", icon: "sitemap", href: "/admin/camps"},
+        {title: "Midburn NPO", icon: "users", children: [
+            {title: "Members", href: "/admin/npo"}
+        ]}
+    ];
+    res.render(name, options);
+};
+
+router.get('/', userRole.isAdmin(), function (req, res) {
+    _adminRender(req, res, 'admin/home.jade', {
+        tiles: [
+            {top: {icon: "user", title: "Total Users"}, count: {text: "250", green: false}, bottom: {title: "Users admin", href: "/admin/users"}},
+            {top: {icon: "sitemap", title: "Total Camps"}, count: {text: "5", green: false}, bottom: {title: "Camps admin", href: "/admin/camps"}}
+            // {top: {icon: "user", title: "Total Users"}, count: {text: "2500", green: false}, bottom: {small: "4%", sort: "", color: "green", title: "From last week"}},
+            // {top: {icon: "clock-o", title: "Average Time"}, count: {text: "123.50", green: false}, bottom: {small: "3%", sort: "asc", color: "green", title: "From last week"}},
+            // {top: {icon: "user", title: "Total Males"}, count: {text: "2,500", green: true}, bottom: {small: "34%", sort: "asc", color: "green", title: "From last week"}},
+            // {top: {icon: "user", title: "Total Females"}, count: {text: "4,567", green: false}, bottom: {small: "12%", sort: "desc", color: "red", title: "From last week"}},
+            // {top: {icon: "user", title: "Total Collections"}, count: {text: "2,315", green: false}, bottom: {small: "4%", sort: "asc", color: "green", title: "From last week"}},
+            // {top: {icon: "user", title: "Total Connections"}, count: {text: "7,325", green: false}, bottom: {small: "56%", sort: "asc", color: "green", title: "From last week"}}
+        ]
+    });
 });
 
-router.get('/npo', userRole.isAdmin(), function (req, res, next) {
+router.get('/users', userRole.isAdmin(), function(req, res) {
+    User.fetchPage({pageSize: 2, page: 1})
+        .then(function(users){
+            rows = [];
+            users.forEach(function(user) {
+                rows.push({
+                    even: true,
+                    cells: [
+                        {text: user.id, last: false, sorting: "asc"},
+                        {text: user.attributes.email, last: false},
+                        {text: user.attributes.first_name, last: false},
+                        {text: user.attributes.last_name, last: true}
+                    ]
+                })
+            });
+            _adminRender(req, res, 'admin/users.jade', {
+                title: {text: "Spark users", small: ""},
+                columns: [
+                    {title: "Id", last: false},
+                    {title: "Email", last: false},
+                    {title: "First name", last: false},
+                    {title: "Last name", last: true}
+                ],
+                rows: rows
+            });
+        })
+    ;
+});
+
+router.get('/npo', userRole.isAdmin(), function (req, res) {
     NpoMember.forge().query({where: {membership_status: NpoStatus.applied_for_membership}})
         .fetchAll({withRelated: ['user']}).then(function (members) {
-        res.render('admin/npo.jade', {members: members.models});
+        _adminRender(req, res, 'admin/npo.jade', {members: members.models});
     });
 });
 
@@ -36,7 +128,7 @@ router.post('/npo', userRole.isAdmin(), function (req, res, next) {
                             if (theUser == null) {
                                 //TODO handle error.
                                 log.error("User not found!");
-                                return res.render('admin/admin_npo', {errorMessage: 'email ' + recipient + ' not found'});
+                                return _adminRender(req, req, 'admin/admin_npo', {errorMessage: 'email ' + recipient + ' not found'});
                             }
                             if (theUser.attributes.membership_status == npoStatus.applied_for_membership) {
                                 theUser.attributes.membership_status = npoStatus.request_approved;
@@ -54,7 +146,7 @@ router.post('/npo', userRole.isAdmin(), function (req, res, next) {
                             else {
                                 //TODO handle error.
                                 log.warn("Incorrect status - ", theUser.attributes.membership_status);
-                                return res.render('admin/admin_npo', {errorMessage: 'email ' + recipient + ' - incorrect status'});
+                                return _adminRender(req, req, 'admin/admin_npo', {errorMessage: 'email ' + recipient + ' - incorrect status'});
                             }
                         });
                     }
