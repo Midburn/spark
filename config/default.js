d6542f0e79e8c47595762fca2fefa37de841ebcc
--unset <nate@konimbo.co.il>
2017-02-16 19:28:27 +0200

    marge-master

--- config/default.js
+++ config/default.js
@@ -1,27 +1,79 @@
 const opsworks = require('../opsworks.js');
 
-module.exports = {
-  database: opsworks.db,
+switch(process.env.SPARK_DB_CLIENT||"sqlite3") {
+  case "mysql":
+    exports.database = {
+      "client"   : process.env.SPARK_DB_CLIENT,
+      "host"     : process.env.SPARK_DB_HOSTNAME,
+      "database" : process.env.SPARK_DB_DBNAME,
+      "user"     : process.env.SPARK_DB_USER,
+      "password" : process.env.SPARK_DB_PASSWORD,
+      "charset"  : "utf8",
+      "debug"    : (process.env.SPARK_DB_DEBUG=="true")
+    };
+    break;
+  
+  case "sqlite3":
+    exports.database = {
+      "client"   : process.env.SPARK_DB_CLIENT||"sqlite3",
+      "filename" : process.env.SPARK_DB_FILENAME||"./dev.sqlite3",
+      "debug"    : (process.env.SPARK_DB_DEBUG=="true")
+    };
+    break;
+  
+  default:
+    console.log("environment variable SPARK_DB_TYPE is configured wrong.");
+    console.log("See .env-example file for more details.");
+    console.log("");
+    process.exit(1);
+}
 
-  server: opsworks.server,
+exports.server = {
+  port      : process.env.SPARK_SERVER_PORT||"3000",
+  hostname  : process.env.SPARK_SERVER_HOSTNAME||"localhost",
+  protocol  : process.env.SPARK_SERVER_PROTOCOL||"http",
+  url       : process.env.SPARK_SERVER_URL||"http://localhost:3000" // full URL including protocol and port. NO trailing slash
+};
 
-  mail: opsworks.mail,
+exports.mail = {
+  enabled         : typeof(process.env.SPARK_MAILSERVER_ENABLE)=="undefined" ? true : (process.env.SPARK_MAILSERVER_ENABLE == "true"),
+  from            : process.env.SPARK_MAILSERVER_FROM||"spark@localhost",
+  host            : process.env.SPARK_MAILSERVER_HOST||"localhost",
+  port            : process.env.SPARK_MAILSERVER_PORT||"25",
+  transportMethod : process.env.SPARK_MAILSERVER_METHOD||"SMTP",           // default is SMTP. Accepts anything that nodemailer accepts
+  secureConnection: (process.env.SPARK_MAILSERVER_SECURE_CONNECTION=="true")
+};
 
-  i18n: {
-    languages: ["he", "en"]
-  },
+if (process.env.SPARK_MAILSERVER_USER) {
+  exports.mail.auth = {
+    user: process.env.SPARK_MAILSERVER_USER,
+    pass: process.env.SPARK_MAILSERVER_PASSWORD
+  }
+}
 
-  payment: opsworks.payment,
+exports.i18n = {
+  languages: ["he", "en"]
+};
 
-  npo: opsworks.npo,
+exports.payment = {
+  iCreditUrl              : process.env.SPARK_ICREDIT_URL,
+  iCreditGroupPrivateToken: process.env.SPARK_ICREDIT_PRIVATETOKEN
+};
 
-  facebook: opsworks.facebook,
+exports.npo = {
+  email         : "amuta@localhost",
+  idImagesFolder: "d:/temp/"
+};
 
-  recaptcha: {
-    sitekey: opsworks.recaptcha.sitekey,
-    secretkey: opsworks.recaptcha.secretkey,
-    ignore: opsworks.recaptcha.ignore
-  },
+exports.facebook = {
+  app_id: process.env.SPARK_FACEBOOK_APP||"1083906121721925",
+  app_secret: process.env.SPARK_FACEBOOK_SECRET,
+  callbackBase: process.env.SPARK_FACEBOOK_CALLBACK||"http://localhost:3000"
+};
 
-  published_camps_origin: 'http://54.194.247.12'
+exports.recaptcha = {
+  ignore: typeof(process.env.SPARK_RECAPTCHA_IGNORE)=="undefined" ? true : (process.env.SPARK_RECAPTCHA_IGNORE=="true"),           // when ignore is true - recaptcha is enabled but if it fails it ignores and continues sign up anyway
+  // TODO change eyalliebermann app in an oficial one
+  sitekey: process.env.SPARK_RECAPTCHA_SITEKEY||"6LcdJwwUAAAAAGfkrUCxOp-uCE1_69AlIz8yeHdj",
+  secretkey: process.env.SPARK_RECAPTCHA_SECRETKEY||"6LcdJwwUAAAAAFdmy7eFSjyhtz8Y6t-BawcB9ApF"
 };
