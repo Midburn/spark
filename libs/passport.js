5a69d26bf4dd7d17668b0d409d36d1d87fff56b8
Leon Fedotov <fedotov.leon@gmail.com>
2017-03-15 22:36:52 +0200

    added spark login from drupal

--- libs/passport.js
+++ libs/passport.js
@@ -5,7 +5,8 @@ var User = require('../models/user').User;
 var DrupalUser = require('../models/user').DrupalUser;
 var facebookConfig = require('config').get("facebook");
 var constants = require('../models/constants');
-var request = require('request');
+var request = require('superagent');
+var _ = require('lodash');
 var spark_drupal_gw_login = function (email, password, done) {
     email = 'asaf@omc.co.il';
     password = 'asiOMC769';
@@ -71,20 +72,15 @@ var spark_drupal_gw_login = function (email, password, done) {
  */
 const drupal_login = (email, password, done) => {
 
-    request({
-        url: 'https://profile-test.midburn.org/api/user/login',
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        form: { 'username': email, 'password': password }
-    }, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            console.log(body);
-            var data = JSON.parse(body);
-            if (body.indexOf('token') > 0) {
-                //{"sessid":"6C55PwkxIyJuKdGtgZ_NQjqxuHASv_kpu1YtaL-1G6A","session_name":"SSESS9516db0c1dfbca7cb87908a6ba2ecabb","token":"E1ceyKMfRLhksPf7gtwJjgm3PG72FutCYOXzbwgPWKU","user":{"uid":"13958","name":"omerpines@hotmail.com","mail":"omerpines@hotmail.com","theme":"","signature":"","signature_format":"filtered_html","created":"1452326800","access":"1489607292","login":1489608167,"status":"1","timezone":null,"language":"he","picture":null,"data":{"contact":0,"mimemail_textonly":0,"ckeditor_default":"t","ckeditor_show_toggle":"t","ckeditor_width":"100%","ckeditor_lang":"en","ckeditor_auto_lang":"t"},"roles":{"2":"authenticated user"},"og_user_node":[],"field_profile_first":{"und":[{"value":"\u05e2\u05d5\u05de\u05e8","format":null,"safe_value":"\u05e2\u05d5\u05de\u05e8"}]},"field_profile_last":{"und":[{"value":"\u05e4\u05d9\u05e0\u05e1","format":null,"safe_value":"\u05e4\u05d9\u05e0\u05e1"}]},"field_profile_phone":{"und":[{"value":"0546501091","format":null,"safe_value":"0546501091"}]},"field_profile_birth_date":{"und":[{"value":"1979-11-08 00:00:00","timezone":"Asia/Jerusalem","timezone_db":"Asia/Jerusalem","date_type":"datetime"}]},"field_profile_playa_name":[],"field_profile_occupation":{"und":[{"value":"\u05de\u05d7\u05e9\u05d1\u05d9\u05dd","format":null,"safe_value":"\u05de\u05d7\u05e9\u05d1\u05d9\u05dd"}]},"field_profile_hobbies":[],"field_profile_burning_man":{"und":[{"value":"1"}]},"field_profile_midburn":{"und":[{"value":"1"}]},"field_profile_participation_more":[],"field_profile_evt_participation":{"und":[{"value":"Theme Camp"}]},"field_profile_nationality":{"und":[{"value":"Israel"}]},"field_field_profile_document_id":{"und":[{"value":"037159506","format":null,"safe_value":"037159506"}]},"field_profile_address":{"und":[{"country":"IL","administrative_area":null,"sub_administrative_area":null,"locality":"\u05ea\u05dc \u05d0\u05d1\u05d9\u05d1","dependent_locality":null,"postal_code":"6791415","thoroughfare":"\u05d4\u05e8\u05d5\u05d2\u05d9 \u05de\u05dc\u05db\u05d5\u05ea 14\u05d0, \u05d3\u05d9\u05e8\u05d4 2","premise":"","sub_premise":null,"organisation_name":null,"name_line":"\u05e2\u05d5\u05de\u05e8 \u05e4\u05d9\u05e0\u05e1","first_name":"\u05e2\u05d5\u05de\u05e8","last_name":"\u05e4\u05d9\u05e0\u05e1","data":null}]},"field_profile_burning_man_years":[],"field_profile_midburn_years":{"und":[{"value":"2014"},{"value":"2015"}]},"field_profile_disability":{"und":[{"value":"0"}]},"field_profile_disability_type":[],"field_profile_disability_other":[],"field_read_survival_guide":{"und":[{"value":"1"}]},"field_read_ten_principles":{"und":[{"value":"1"}]},"field_terms_and_conditions":{"und":[{"value":"1"}]},"field_medical_training":{"und":[{"value":"0"}]},"field_medical_experience":[],"field_profile_admin_comments":[],"field_profile_arrive_early":{"und":[{"value":"0"}]},"field_sex":{"und":[{"value":"Male"}]},"field_signup_for_our_newsletter":{"und":[[]]},"field_signup_for_our_newsletter2":{"und":[[]]},"field_amuta_membership_status":[],"field_amuta_membership_date":{"und":[{"value":null,"timezone":"Asia/Jerusalem","timezone_db":"Asia/Jerusalem","date_type":"datestamp"}]},"field_profile_disabled_parking":{"und":[{"value":"0"}]},"field_foreign_passport":[]}}
-            }
-        }
-    });
+    return request
+        .post('https://profile-test.midburn.org/api/user/login')
+        .send({ 'username': email, 'password': password })
+        .set('Accept', 'application/json')
+        .set('Content-Type': 'application/x-www-form-urlencoded')
+        .then(((data) => (~data.indexOf('token')) ? JSON.parse(data) : null), (error) => console.error(error))
+
+
+
     // DrupalUser.forge({
     //     name: email
     // }).fetch().then(function (drupalUser) {
@@ -114,20 +110,28 @@ const drupal_login = (email, password, done) => {
 var login = function (email, password, done) {
     // spark_drupal_gw_login(email,password,done);
     drupal_login(email, password).then(function (drupal_user) {
-        if (drupal_user != null && drupal_user.authenticated) {
+        if (drupal_user != null) {
             new User({
                 email: email
             }).fetch().then(function (user) {
                 if (user === null) {
-                    done(false, i18next.t('user_not_validated', {
-                        email: email
-                    }));
-                    //     // no corresponding spark user is found
-                    //     // try to get a drupal user - once drupal user is logged-in a corresponding spark user is created
-                    //     // on next login - there will be a spark user, so drupal login will not be attempted again
-                    //     // drupal_login(email, password, done);
-                    // // } else if (!user.validPassword(password)) {
-                    // //     done(false, i18next.t('invalid_user_password'));
+
+                    // done(false, i18next.t('user_not_validated', {
+                    //     email: email
+                    // }));
+                    signup(email, 'you bet!', {
+                        first_name: _.get(user, 'user.field_profile_first.und.value', ''),
+                        last_name: _.get(user, 'user.field_profile_last.und.value', ''),
+                        cell_phone: _.get(user, 'user.field_profile_phone.und.value', 666),
+                        gender: constants.USER_GENDERS_DEFAULT,
+                        validated: true
+                    }, function (newUser, error) {
+                        if (newUser) {
+                            done(newUser);
+                        } else {
+                            done(false, error);
+                        }
+                    });
                 } else if (!user.attributes.validated) {
                     done(false, i18next.t('user_not_validated', {
                         email: email
