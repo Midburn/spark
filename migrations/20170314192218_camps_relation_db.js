b04e74354bbd2dee9e59b923b2ec2380cfd5d70b
Asaf Azulay <asi@omcdom.hq.omc.co.il>
2017-03-15 08:35:15 +0200

    dbs

---
+++ migrations/20170314192218_camps_relation_db.js
@@ -0,0 +1,51 @@
+var constants = require('../models/constants.js');
+
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.alterTable(constants.CAMPS_TABLE_NAME, function (table) {
+            if (!knex.schema.hasColumn(constants.CAMPS_TABLE_NAME,'event_id')) {
+                table.string('event_id', 15);
+                table.dropUnique('camp_name_he');
+                table.dropUnique('camp_name_en');
+                table.unique(['event_id', 'camp_name_en']);
+                table.unique(['event_id', 'camp_name_he']);
+            }
+        }),
+        // knex.schema.table(constants.CAMP_DETAILS_TABLE_NAME, function (table) {
+        //     table.dropColumn('camp_id');
+        // }),
+        knex.schema.alterTable(constants.CAMP_DETAILS_TABLE_NAME, function (table) {
+            // table.integer('camp_id').unsigned();
+            table.unique(['camp_id']);
+        }),
+        knex.schema.createTable(constants.CAMP_MEMBERS_TABLE_NAME, function (table) {
+            table.integer('camp_id').unsigned();
+            table.integer('user_id').unsigned();
+            table.unique(['user_id']);
+            table.index('camp_id');
+            table.foreign('camp_id').references(constants.CAMPS_TABLE_NAME + '.id');
+            table.foreign('user_id').references(constants.USERS_TABLE_NAME + '.user_id');
+            table.enu('status', constants.CAMP_MEMBER_STATUS);
+            table.text('addinfo_json', 'mediumtext');
+        }),
+
+        knex.schema.createTable('props', function (table) {
+            table.string('prop_id', 50);
+            table.string('group_id', 50);
+            table.text('data_json');
+            table.unique(['group_id', 'prop_id']);
+            table.index('prop_id');
+        })
+    ]);
+};
+
+exports.down = function (knex, Promise) {
+    return Promise.all([
+        knex.schema.dropTable('props'),
+        knex.schema.dropTable(constants.CAMP_MEMBERS_TABLE_NAME),
+        // knex.schema.table(constants.CAMPS_TABLE_NAME,function(table){
+        //     table.dropColumn('event_id');
+        // }),
+    ]);
+};
+
