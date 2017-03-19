a99bdee648aebf8fb39434b7e1b746a1ad1d4064
Leon Fedotov <fedotov.leon@gmail.com>
2017-03-19 22:52:39 +0200

    remove code duplication

--- libs/passport.js
+++ libs/passport.js
@@ -7,53 +7,6 @@ var constants = require('../models/constants')
 var request = require('superagent')
 var _ = require('lodash')
 
-var spark_drupal_gw_login = function (email, password, done) {
-  request({
-    url: 'https://profile-test.midburn.org/api/user/login',
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    form: { 'username': email, 'password': password }
-  }, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      if (body.indexOf('token') > 0) {
-        // user logged in good
-        var userPromise = new User({
-          email: email
-        }).fetch()
-        userPromise.then(function (model) {
-          if (model) {
-            done(false, i18next.t('user_exists'))
-          } else {
-            var newUser = new User({
-              email: email,
-              first_name: user.first_name,
-              last_name: user.last_name,
-              gender: user.gender,
-              validated: user.validated
-            })
-            if (password) {
-              newUser.generateHash(password)
-            }
-            if (!user.validated) {
-              newUser.generateValidation()
-            }
-            newUser.save().then(function (model) {
-              done(newUser)
-            })
-          }
-        })
-        done(newUser)
-        //res.status(200).jsonp({ status: 'true', 'massage': 'user authorized', 'data': data })
-      } else {
-        done(false, 'unknown')
-      }
-    } else {
-      done(false, 'unknown')
-      //res.status(401).jsonp({ status: 'false', 'massage': 'Not authorized!!!' })
-    }
-  })
-}
-
 /***
  * tries to login based on drupal users table
  * once user is successfully logged-in, an automatic sign-up flow is performed which creates a corresponding spark user
@@ -71,7 +24,6 @@ const drupal_login = (email, password, done) =>
 
 
 var login = function (email, password, done) {
-  spark_drupal_gw_login(email, password, done)
   drupal_login(email, password).then(function (drupal_user) {
     if (drupal_user != null) {
       new User({
