a268398f489e21c621638d12deed1e9dd5393438
Netanel Ben <mtr574@users.noreply.github.com>
2017-03-24 16:07:13 +0300

    Update members.js

--- public/scripts/controllers/members.js
+++ public/scripts/controllers/members.js
@@ -1,5 +1,5 @@
 app.controller("membersController", function($scope, $http) {
-    var camp_id = 167;
+    var camp_id = 167; // for admin: should retreive camp_id from camps select -- for camp_manager: his own camp_id;
     function _getMembers() {
         $http.get('/camps/' + camp_id + '/members').then(function(res) {
             $scope.members = res.data.members;
