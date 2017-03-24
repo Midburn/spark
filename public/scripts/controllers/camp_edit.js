c18c96ff541aed9a1c30eeda5434cd63a7232438
guy.warburg@jira.midburn.org <guy.warburg@gmail.com>
2017-03-24 18:19:06 +0300

    basic members implementation

--- public/scripts/controllers/camp_edit.js
+++ public/scripts/controllers/camp_edit.js
@@ -1,12 +1,13 @@
 app.controller("campEditController", function($scope, $http) {
     var camp_id = document.querySelector('#meta__camp_id').value;
-    
     function _getMembers() {
-        $http.get('/users').then(function(res) {
-            $scope.members = res.data.users;
+        $http.get('/camps/' + camp_id + '/members').then(function(res) {
+            $scope.members = res.data.members;
         });
     }
-    
+    $scope.changeOrderBy = function(orderByValue) {
+        $scope.orderMembers = orderByValue;
+    }
     if (typeof camp_id !== 'undefined') {
         _getMembers();
     }
