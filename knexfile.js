2e20532fef61415bca0c517549076063c5a11af8
Asaf Azulay <asi@omcdom.hq.omc.co.il>
2017-03-28 14:29:04 +0300

    sdfsadf

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,9 @@
-// Update with your config settings.
+var config = require('config');
+var dbConfig = config.get('database');
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+    client: dbConfig.client,
+    connection: dbConfig,
+    debug: dbConfig.debug,
+    useNullAsDefault: true
 };
