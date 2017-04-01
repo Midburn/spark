84775605a68bdc2949e72b32c5cb3799bfc1ac26
Guy Warburg <guy.warburg@gmail.com>
2017-04-01 13:59:41 +0300

    Translated Sweet-Alert pop-ups (#232)
    * camps route fix
    
    * Revert "camps route fix"
    
    This reverts commit d926484a77b981f08ec68501acf61992c1621d05.
    
    * translated join request errors
    
    * translated edit file
    
    * final translations
    
    * lint

--- public/scripts/controllers/camp_edit.js
+++ public/scripts/controllers/camp_edit.js
@@ -47,14 +47,19 @@ app.controller("campEditController", function($scope, $http, $filter) {
     }
     $scope.updateUser = (user_name, user_id, action_type) => {
         var camp_id = document.getElementById('meta__camp_id').value;
-        sweetAlert({
-            title: "Are you sure?",
-            text: "Are you sure you would like to " + action_type + " " + user_name + "?",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes",
-            closeOnConfirm: false
+        var lang = document.getElementById('meta__lang').value;
+
+        if (lang === 'he') {
+            updateUserHE(camp_id);
+        } else {
+            sweetAlert({
+                title: "Are you sure?",
+                text: "Are you sure you would like to " + action_type + " " + user_name + "?",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes",
+                closeOnConfirm: false
             },
             function() {
                 var request_str = `/camps/${camp_id}/members/${user_id}/${action_type}`
@@ -67,9 +72,34 @@ app.controller("campEditController", function($scope, $http, $filter) {
                     _getMembers();
                 }).catch((err) => {
                     console.log(err.message);
-                     sweetAlert("Error!", "Something went wrong, please try again later", "error");
-                })
-                
+                    sweetAlert("Error!", "Something went wrong, please try again later", "error");
+                })     
+            });
+        }
+    }
+
+    updateUserHE = (camp_id) => {
+        sweetAlert({
+                title: "האם את/ה בטוח?",
+                text: "האם את/ה בטוח שתרצה לבצע " + action_type + " את משתמש " + user_name + "?",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "Yes",
+                closeOnConfirm: false
+            },
+            function() {
+                var request_str = `/camps/${camp_id}/members/${user_id}/${action_type}`
+                $http.get(request_str).then((res) => {
+                    sweetAlert(action_type + "!", "משתמש " + user_name + action_type, " בהצלחה");
+                    // TODO - check if table update is needed
+                    setTimeout(() => {
+                        innerHeightChange()
+                    }, 500)
+                }).catch((err) => {
+                    console.log(err.message);
+                    sweetAlert("שגיאה!", "הייתה תקלה, נסה/י שוב מאוחר יותר", "error");
+                })     
             });
     }
 });
