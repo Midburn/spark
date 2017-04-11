df7849f28fd26975d0269f0b3eb8d5ccd93205d9
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-11 21:55:28 +0300

    working on testing adding a single user - still getting 403 forbidden

--- tests/routes/volunteers_routes.test.js
+++ tests/routes/volunteers_routes.test.js
@@ -3,6 +3,13 @@ const should = require('chai').should();
 const request = require('supertest')(app);
 
 
+const testUser1 = {
+    id: 1,
+    email: 'it@midburn.com',
+    first_name: 'Master',
+    last_name: 'burner'
+};
+
 describe('Tests are fine', function () {
     it('responds to / with redirect to hebrew', function testSlash(done) {
         request
@@ -83,7 +90,7 @@ describe('Getters all respond', function () {
 
                 const volunteer = res.body[0];
                 volunteer.should.have.property('department_id');
-                volunteer.should.have.property('user_id'); //TODO should not be null
+                volunteer.should.have.property('user_id'); //TODO test should not be null
                 volunteer.should.have.property('first_name');
                 volunteer.should.have.property('last_name');
                 volunteer.should.have.property('email');
@@ -106,8 +113,30 @@ describe('Getters all respond', function () {
     });
 });
 
-describe.skip('Volunteers addition', function () {
+describe('Adding volunteers', function () {
     //ading one
+    it('should add a single volunteer successfully', function (done) {
+        //get number of volunteers and veridy our volunteer is not already there
+        const departmentId = 2;
+        request
+            .post(`/volunteers/departments/${departmentId}/volunteers/`)
+            .type('application/json')
+            .accept('application/json')
+            .send( //JSON.stringify(
+                {
+                    email: testUser1.email,
+                    role_id: 9999,
+                    is_production: true
+                }
+                //)
+            )
+            .expect(200)
+            .expect([{
+                "email": testUser1.email,
+                "status": "OK"
+            }], done);
+
+    });
     //add and get
     //collision returns error and does not change existing
     //extra fields are rejected
