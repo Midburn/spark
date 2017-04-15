2b75564b209c6389624732e1e8406cdd3d4e4ed6
Ori Hoch <ori@uumpa.com>
2017-04-15 14:31:46 +0300

    codeship: yarn fixes

--- .codeship/setup.sh
+++ .codeship/setup.sh
@@ -4,5 +4,6 @@ nvm install
 nvm use
 curl -o- -L https://yarnpkg.com/install.sh | bash
 export PATH="$HOME/.yarn/bin:$PATH"
+yarn install
 yarn add bower
 ./node_modules/.bin/bower install
--- .codeship/test.sh
+++ .codeship/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 ./node_modules/.bin/knex migrate:latest
-npm test
+yarn test
