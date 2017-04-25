577147a62bd07566c119d06ebbba5d6db4cdbc6e
רועי צחור <roy.zahor@gmail.com>
2017-04-25 17:08:07 +0300

    fixed NPO migration

--- migrations/20170422204738_npo.js
+++ migrations/20170422204738_npo.js
@@ -1,19 +1,28 @@
 exports.up = function (knex, Promise) {
     return Promise.all([
 
-        knex.schema.alterTable('npo_members', function (table) {
+        knex.schema.table('npo_members', function (table) {
             table.integer('member_number').unsigned();
             table.binary('document_image');
             table.text('application_data');
             table.integer('application_reviewer_id').unsigned();
         }),
 
-        knex.schema.alterTable('npo_members', function (table) {
+        knex.schema.table('npo_members', function (table) {
             table.foreign('application_reviewer_id').references('users.user_id');
         })
     ]);
 };
 
 exports.down = function (knex, Promise) {
+    return Promise.all([
 
+        knex.schema.table('npo_members', function (table) {
+            table.dropForeign('application_reviewer_id');
+            table.dropColumn('member_number');
+            table.dropColumn('document_image');
+            table.dropColumn('application_data');
+            table.dropColumn('application_reviewer_id');
+        })
+    ]);
 };
