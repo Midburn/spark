2bf05ff40b6313e92730709af96482b574acec99
Noam Almog <noama@wix.com>
2017-05-01 12:07:11 +0300

    do not authorize static resources

--- libs/auth-express.js
+++ libs/auth-express.js
@@ -3,11 +3,11 @@ const unless = require('express-unless');
 
 const devApi = path => path.startsWith('/dev');
 const loginUrl = path => path.startsWith('/jwt-login') || path.endsWith('/login') || path.endsWith('/signup') || path === '/';
-// const staticContent = path => path.startsWith('/bower_components') || path.startsWith('/scripts');
+const staticContent = path => path.startsWith('/bower_components') || path.startsWith('/scripts');
 
 const containLoginToken = req => (req.cookies && req.cookies[auth.SessionCookieName]) || (req.headers && req.headers['authorization']) || false;
 
-const shouldProtectApi = req => devApi(req.path) || (loginUrl(req.path) && !containLoginToken(req));
+const shouldProtectApi = req => staticContent(req.path) || devApi(req.path) || (loginUrl(req.path) && !containLoginToken(req));
 
 module.exports = app => {
     app.use(auth.initialize());
