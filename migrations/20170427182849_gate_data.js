971b99b77424fa7c24fc83f8715f6cb79d6f0137
רועי צחור <roy.zahor@gmail.com>
2017-05-03 15:33:00 +0300

    Working include everything to sync from Drupal

--- migrations/20170427182849_gate_data.js
+++ migrations/20170427182849_gate_data.js
@@ -1,7 +1,12 @@
 
 exports.up = function(knex, Promise) {
     return Promise.all([
-        knex('events').insert({event_id: "MIDBURN_2017"})
+        knex('events').insert({event_id: "MIDBURN_2017"}),
+
+        knex.schema.table('events', function (table) {
+            table.string('name', 128);
+            table.string('gate_code', 100);
+        })
     ]);
 };
 
