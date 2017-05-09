d573b1910d7afb64d5530ddd7c252a22bf35d268
רועי צחור <roy.zahor@gmail.com>
2017-05-09 14:07:07 +0300

    Migrations fix

--- migrations/20170509123041_gate_camps_data_migration.js
+++ migrations/20170509123041_gate_camps_data_migration.js
@@ -34,35 +34,37 @@ exports.up = function (knex, Promise) {
             "DELETE FROM users_groups WHERE group_id = old.id; " +
             "END"),
 
-        knex.select().from('camps').then(camps => {
+        knex.select().from('camps').leftJoin('users_groups', 'id', 'group_id').then(camps => {
             if (camps) {
                 console.log("Found", camps.length, "camps to migrate");
                 _.each(camps, camp => {
-                    console.log("Camp:", camp.camp_name_he);
-                    knex('users_groups').insert({
-                        group_id: camp.camp_id,
-                        event_id: "MIDBURN2017",
-                        type: "CAMP",
-                        name: camp.camp_name_he,
-                        created_at: new Date()
-                    }).then();
+                    if (camp.id && !camp.group_id) {
+                        console.log("Camp:", camp.id, camp.camp_name_he);
+                        knex('users_groups').insert({
+                            group_id: camp.id,
+                            event_id: "MIDBURN2017",
+                            type: "CAMP",
+                            name: camp.camp_name_he,
+                            created_at: new Date()
+                        }).then();
+                    }
                 })
             }
         }),
 
-        knex.select().from('camp_members').leftJoin('users_groups_membership', function () {
-            this.on("camp_members.user_id", "=", "users_groups_membership.user_id")
-            .on("users_groups_membership.group_id", "=", "camp_members.camp_id");
-        })
+        knex.raw("select cm.camp_id, users_groups_membership.user_id " +
+            "from `camp_members` cm  left join `users_groups_membership` " +
+            "on `cm`.`user_id` = `users_groups_membership`.`user_id` " +
+            "and `users_groups_membership`.`group_id` = `cm`.`camp_id`")
             .then(camp_members => {
                 if (camp_members) {
                     console.log("Found", camp_members.length, "camp members to migrate");
                     _.each(camp_members, member => {
                         if (member.camp_id && !member.group_id) {
-                            console.log("member:", member.user_id, "group:", member.camp_id);
+                            console.log("member:", member['cm.user_id'], "group:", member.camp_id);
                             knex('users_groups_membership').insert({
                                 group_id: member.camp_id,
-                                user_id: member.user_id,
+                                user_id: member['cm.user_id'],
                                 created_at: new Date()
                             }).then(() => {
                                 console.log("OK")
