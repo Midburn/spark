95af8270c63d6f67d69756ad949437a5b99a8317
Noam Almog <noama@wix.com>
2017-05-10 21:54:06 +0300

    update redirect logic

--- routes/volunteering_routes.js
+++ routes/volunteering_routes.js
@@ -1,21 +1,9 @@
-var express = require('express');
-var router = express.Router({
-    mergeParams: true
-});
-var passportLib = require('../libs/passport');
-var userRole = require('../libs/user_role');
-var User = require('../models/user').User;
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const userRole = require('../libs/user_role');
 
 router.get('/', userRole.isLoggedIn(), function (req, res) {
-    User.forge({user_id: req.user.attributes.user_id}).fetch().then(user => {
-        if (user) {
-            var token = passportLib.generateJwtToken(user.attributes.email);
-            res.redirect("http://URL.OF.VOLUNTEERING.SYSTEM.LANDING.PAGE?auth_token=" + token);
-        }
-        else {
-            res.status(500).send("Redirect error");
-        }
-    });
+    res.redirect('http://URL.OF.VOLUNTEERING.SYSTEM.LANDING.PAGE');
 });
 
 module.exports = router;
