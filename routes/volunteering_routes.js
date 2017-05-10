a3085caa83623fc2e66c050d8e488be89cca3fbc
Noam Almog <noama@wix.com>
2017-05-10 21:55:52 +0300

    update redirect for volunteers

--- routes/volunteering_routes.js
+++ routes/volunteering_routes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const userRole = require('../libs/user_role');
 
+
 router.get('/', userRole.isLoggedIn(), function (req, res) {
-    res.redirect('http://URL.OF.VOLUNTEERING.SYSTEM.LANDING.PAGE');
+    res.redirect(process.env.VOLUNTEERS_BASE_URL);
 });
 
 module.exports = router;
