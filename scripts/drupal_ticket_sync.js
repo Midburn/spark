3620c2bc6f2d13573c3c9819345a0a74ede18950
Leon Fedotov <fedotov.leon@gmail.com>
2017-05-17 00:57:31 +0300

    update page start

--- scripts/drupal_ticket_sync.js
+++ scripts/drupal_ticket_sync.js
@@ -242,7 +242,7 @@ async function syncTickets(fromDate, callback) {
         var session = await getDrupalSession();
         if (session) {
             log.info('Got Drupal session...');
-            var page = 42;
+            var page = 0;
             var running = true;
             while (running) {
                 log.info("Page:", page);
