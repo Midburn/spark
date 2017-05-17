bb6aca057d49f46294fda2a4b26af9ae3dce8db2
Nate.Ben <mtr574@gmail.com>
2017-05-17 22:48:47 +0300

    mig: tickets::disabledParking-bool

--- migrations/20170517223359_tickets_disable_parking.js
+++ migrations/20170517223359_tickets_disable_parking.js
@@ -1,13 +1,11 @@
 exports.up = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.table('tickets', function(table) {
-            table.boolean('disabledParking').defaultTo(false);
-        }
-    ]);};
+    knex.schema.table('tickets', function(table) {
+        table.boolean('disabledParking').defaultTo(false);
+    });
+};
 
 exports.down = function(knex, Promise) {
-    return Promise.all([
-        knex.schema.table('tickets', function(table) {
-            table.dropColumn('disabledParking');
-        }
-    ]);};
+    knex.schema.table('tickets', function(table) {
+        table.dropColumn('disabledParking');
+    });
+};
