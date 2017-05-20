496554a34c6d42f93fadbbe68815f320ea2c58b1
Leon Fedotov <fedotov.leon@gmail.com>
2017-05-20 12:46:19 +0300

    updating migrations

--- migrations/20170519184911_missing_trigger.js
+++ migrations/20170519184911_missing_trigger.js
@@ -1,21 +1,22 @@
 var constants = require('../models/constants');
 
 exports.up = function (knex, Promise) {
-    return Promise.all([
-
-        knex.raw(
-            "CREATE TRIGGER camp_members_groups_after_ins AFTER INSERT ON camp_members " +
-            "FOR EACH ROW " +
-            "BEGIN " +
-            "INSERT INTO users_groups_membership (group_id, user_id, status) VALUES (new.camp_id, new.user_id, new.status); " +
-            "END"),
-
-        knex.schema.table("events", table => {
-            table.enu("gate_status", constants.EVENT_GATE_STATUS);
-        })
-    ])
+  return Promise.all([
+    knex.raw('DROP TRIGGER camp_members_groups_after_ins').then(
+      knex.raw(
+        "CREATE TRIGGER camp_members_groups_after_ins AFTER INSERT ON camp_members " +
+        "FOR EACH ROW " +
+        "BEGIN " +
+        "INSERT INTO users_groups_membership (group_id, user_id, status) VALUES (new.camp_id, new.user_id, new.status); " +
+        "END"
+      )
+    ),
+    knex.schema.table("events", table => {
+      table.enu("gate_status", constants.EVENT_GATE_STATUS);
+    })
+  ])
 };
 
 exports.down = function (knex, Promise) {
-    return Promise.all([]);
+  return Promise.all([]);
 };
