efed73d4f5386d07c672e68d11bb24c036a4c712
Leon Fedotov <LeonFedotov@users.noreply.github.com>
2017-10-06 01:53:54 +0300

    Update 20171006005624_sandbox2017.js

--- migrations/20171006005624_sandbox2017.js
+++ migrations/20171006005624_sandbox2017.js
@@ -7,7 +7,7 @@ exports.up = function (knex, Promise) {
     return Promise.all([
         knex('events').insert({
             event_id: "SANDBOX2017",
-            gate_code: "171819",
+            gate_code: "171820",
             name: "Sandbox 2017 ארגז חול",
             gate_status: "early_arrival"
         })
