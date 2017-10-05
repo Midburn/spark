6587e2aec25a996dff62ffb3c639457d1df77f0c
Leon Fedotov <fedotov.leon@gmail.com>
2017-10-06 01:00:09 +0300

    adding sandbox change migration

---
+++ migrations/20171006005624_sandbox2017.js
@@ -0,0 +1,15 @@
+
+exports.down = function(knex, Promise) {
+
+};
+
+exports.up = function (knex, Promise) {
+    return Promise.all([
+        knex('events').insert({
+            event_id: "SANDBOX2017",
+            gate_code: "171819",
+            name: "Sandbox 2017 ארגז חול",
+            gate_status: "early_arrival"
+        })
+    ]);
+};
