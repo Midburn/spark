391537478d26733b884db8ac881ac1103e9f53fc
Artyom Katsap <artyom.katsap@gmail.com>
2017-11-20 22:13:00 +0200

    added presale_quota to camps table

---
+++ migrations/20171120220422_camps_add_presale_tickets_quota.js
@@ -0,0 +1,13 @@
+const constants = require('../models/constants')
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+        knex.schema.table(constants.CAMPS_TABLE_NAME, function (table) {
+            table.integer("pre_sale_tickets_quota").unsigned();
+        })
+    ]);
+};
+
+exports.down = function(knex, Promise) {
+  
+};
