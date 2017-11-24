825737b136e041588062e6d8b24996602fff2b2d
Artyom Katsap <artyom.katsap@gmail.com>
2017-11-24 19:17:59 +0200

    index view initial layout

--- public/scripts/ng_app.js
+++ public/scripts/ng_app.js
@@ -4,3 +4,4 @@
  * scripts/controllers folder for components logic
  */
 app = angular.module("ngCamps", ['ngAnimate', 'ngSanitize', 'ui.select']); // eslint-disable-line
+angular.module("ngEvents", ['ngAnimate', 'ngSanitize', 'ui.select']); // eslint-disable-line
