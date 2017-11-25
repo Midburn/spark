bc25952a2b64b105200a7c784789f648d6a8ea85
Lior Kaplan <kaplanlior@gmail.com>
2017-11-25 18:54:43 +0200

    Deploy directly on staging server, without going though slack file upload (Closes: #480)

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
 
 source .travis/functions.sh
 
@@ -6,12 +6,28 @@ if [ "${SKIP_BUILD}" != "1" ]; then
     _build_package || _exit_error "error in build stage"
 fi
 
-if [ "${SKIP_UPLOAD}" != "1" ]; then
-    _upload_package || _exit_error "error in upload stage"
+if [ "$TRAVIS_REPO_SLUG" = "Midburn/spark" ]; then
+	echo "An official repository, yey!"
+	if [ -n "$SPARK_DEPLOYMENT_KEY" ]; then
+		if [ "$TRAVIS_BRANCH" = "master" ]; then
+			echo "Deploying to staging server from $TRAVIS_BRANCH branch"
+			echo -e $SPRAK_DEPLOYMENT_KEY | base64 -d > stage_machine.key
+			chmod 400 stage_machine.key
+			scp -o StrictHostKeyChecking=no -i stage_machine.key `_get_deployment_package_filename` "${SPARK_DEPLOYMENT_HOST}:/opt/spark/package.tar.gz"
+			RC=$?
+			if [ $RC -eq 0 ]; then
+				if ssh -o StrictHostKeyChecking=no -i stage_machine.key ${SPARK_DEPLOYMENT_HOST} "/opt/spark/deploy.sh"; then
+					_exit_success "deployment completed successfully"
+				fi
+			rm -f stage_machine.key
+			exit $RC
+		else
+			echo "We don't deploy from $TRAVIS_BRANCH"
+		fi
+	else
+		echo "Can't find deployment SSH key"
+	fi
+else
+	echo "Not the oficial repository, not deploying"
 fi
 
-if [ "${SKIP_DEPLOY}" != "1" ]; then
-    _deploy || _exit_error "error in deploy stage"
-fi
-
-_exit_success "deployment completed successfully"
