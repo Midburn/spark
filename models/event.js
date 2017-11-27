5ce3602cfd09d2d93bbdb5b6b4546b46760748d5
Dima Grossman <dima@grossman.io>
2017-11-27 21:05:38 +0200

    #495 added get_event method

--- models/event.js
+++ models/event.js
@@ -12,6 +12,31 @@ var Event = bookshelf.Model.extend({
     loadProps: function() {
         // this.attributes.json_data;
     }
+
+}, {
+    /**
+     * Returns event by id if found in constants.events object
+     * if the event does not exists it will return the database
+     * version and save it locally to the constants
+     * @param { String } id
+     * @returns {Promise<any>}
+     */
+    get_event: function (id) {
+        if (constants.events[id]) return Promise.resolve(constants.events[id]);
+
+        return Event.where('event_id', id).fetch().then((response) => {
+            if (response.attributes && response.attributes.addinfo_json) {
+                constants.events[id] = {
+                    props: JSON.parse(response.attributes.addinfo_json),
+                    bundles: []
+                };
+
+                return constants.events[id];
+            }
+
+            return null;
+        });
+    }
 });
 
 module.exports = {
