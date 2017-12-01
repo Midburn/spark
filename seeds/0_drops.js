782a0acffdd06069ff2dac83833ee1783540a070
Elad <eladla@softimize.co>
2017-12-01 14:23:15 +0200

    Added seeding for the Midburn 2018 event

--- seeds/0_drops.js
+++ seeds/0_drops.js
@@ -1,10 +1,10 @@
-exports.seed = function(knex, Promise) {
-    return knex('camps').del()
-    .then(() => {
-        return knex('users').del();
-    })
-    .catch((error) => {
+exports.seed = async function(knex, Promise) {
+    try {
+        await knex('camps').del()
+        await knex('users').del()
+        await knex('events').del()
+    } catch (error) {
         log.error('Spark encountered an error while seeding and droping tables:');
         log.error(error);
-    });
+    }
 };
---
+++ seeds/dev/events.js
@@ -0,0 +1,6 @@
+module.exports = [{
+    event_id: 'MIDBURN2018',
+    name: 'Midbrun 2018 מידברן',
+    gate_code: 171822,
+    gate_status: 'regular'
+}]
