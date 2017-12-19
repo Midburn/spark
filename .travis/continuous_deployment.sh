af9b9c5970d406d16ebe6ce6a24f7d2e120d9039
Ori Hoch <ori@uumpa.com>
2017-12-19 19:47:37 +0200

    finalize continuous deployment flow

--- .travis/continuous_deployment.sh
+++ .travis/continuous_deployment.sh
@@ -7,7 +7,7 @@ then
     openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in secret-midburn-k8s-ops.json.enc -out secret-midburn-k8s-ops.json -d
     OPS_REPO_SLUG="OriHoch/midburn-k8s"
     OPS_REPO_BRANCH="finalize-external-app-continuous-deployment-flow"
-    wget https://raw.githubusercontent.com/${OPS_REPO_SLUG}/${OPS_REPO_BRANCH}/run_docker_ops.sh &&
+    wget https://raw.githubusercontent.com/${OPS_REPO_SLUG}/${OPS_REPO_BRANCH}/run_docker_ops.sh
     chmod +x run_docker_ops.sh
     IMAGE_TAG="gcr.io/uumpa123/spark:${TRAVIS_COMMIT}"
     B64_UPDATE_VALUES=`echo '{"spark":{"image":"'${IMAGE_TAG}'"}}' | base64 -w0`
