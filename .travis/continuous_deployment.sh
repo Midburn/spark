dced0a82364a308ea4663eb9f94e4112562268b9
Ori Hoch <ori@uumpa.com>
2017-12-28 11:41:13 +0200

    travis - staging environment

--- .travis/continuous_deployment.sh
+++ .travis/continuous_deployment.sh
@@ -4,7 +4,7 @@ if [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] 
    [ "${TRAVIS_BRANCH}" == "${DEPLOY_BRANCH}" ] &&\
    [ "${TRAVIS_COMMIT_MESSAGE}" != "" ] && ! echo "${TRAVIS_COMMIT_MESSAGE}" | grep -- --no-deploy && [ "${TRAVIS_COMMIT}" != "" ]
 then
-    openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in ./secret-midburn-k8s-ops.json.enc -out ./secret-midburn-k8s-ops.json -d
+    openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in ./midburn-k8s-ops.json.enc -out ./secret-midburn-k8s-ops.json -d
     OPS_REPO_SLUG="Midburn/midburn-k8s"
     OPS_REPO_BRANCH="master"
     IMAGE_TAG="gcr.io/uumpa123/spark:${TRAVIS_COMMIT}"
