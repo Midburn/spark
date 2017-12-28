1721033544e08b6743542cb815d530202ed0078c
yanipo5 <[yip528@gmail.com]>
2017-12-28 14:17:56 +0200

    event coding style

--- routes/api_events_routes.js
+++ routes/api_events_routes.js
@@ -1,6 +1,6 @@
 
 var log = require('../libs/logger')(module);
-const Event = require('../models/event').Event
+const Event = require('../models/event').Event;
 const userRole = require('../libs/user_role');
 const _ = require('lodash')
 /*
@@ -12,8 +12,7 @@ name,
 gate_code,
 gate_status
 */
-var createEvent = function(req) 
-{
+var createEvent = function (req) {
     var new_event = {
         event_id: _.get(req, 'body.event_id'),
         ext_id_event_id: _.get(req, 'body.ext_id_event_id'),
@@ -30,55 +29,55 @@ module.exports = (app, passport) => {
 
     app.get('/events', (req, res) => {
         Event.fetchAll()
-        .then((events) => {
-            res.status(200).json(
-                { 
-                    events: events.toJSON()
-                }
-            )
-        })
-        .catch((err) => {
-            res.status(500).json({
-                error: true,
-                data: {
-                    message: err.message
-                }
+            .then((events) => {
+                res.status(200).json(
+                    {
+                        events: events.toJSON()
+                    }
+                )
+            })
+            .catch((err) => {
+                res.status(500).json({
+                    error: true,
+                    data: {
+                        message: err.message
+                    }
+                });
             });
-        });
     });
 
-    app.post('/events/new', 
-        [userRole.isLoggedIn(), userRole.isAllowNewCamp()],
+    app.post('/events/new',
+        [userRole.isLoggedIn(), userRole.isAdmin()],
         (req, res) => {
             var new_event = createEvent(req);
             Event.forge().save(new_event)
-            .then(res.send(200))
-            .catch((e) => {
-                res.status(500).json({
-                    error: true,
-                    data: {
-                        message: e.message
-                    }
+                .then(res.send(200))
+                .catch((e) => {
+                    res.status(500).json({
+                        error: true,
+                        data: {
+                            message: e.message
+                        }
+                    });
                 });
-            });
-        }); 
+        });
 
-        app.put('/events/update', 
-        [userRole.isLoggedIn(), userRole.isAllowNewCamp()],
+    app.put('/events/update',
+        [userRole.isLoggedIn(), userRole.isAdmin()],
         (req, res) => {
             var new_event = createEvent(req);
             var event_id = new_event.event_id;
-            Event.forge({event_id: event_id}).save(new_event)
-            .then(res.send(200))
-            .catch((e) => {
-                res.status(500).json({
-                    error: true,
-                    data: {
-                        message: e.message
-                    }
+            Event.forge({ event_id: event_id }).save(new_event)
+                .then(res.send(200))
+                .catch((e) => {
+                    res.status(500).json({
+                        error: true,
+                        data: {
+                            message: e.message
+                        }
+                    });
                 });
-            });
-        }); 
+        });
 
     app.get('/events/:event_id',
         [userRole.isLoggedIn()],
@@ -92,13 +91,14 @@ module.exports = (app, passport) => {
                 for (var prop in props) { events.attributes[prop] = props[prop]; }
                 console.log(event);
                 res.json({ event: event });
-            });    
+            });
         });
 
-    app.put('/events/:event/edit', 
-        userRole.isLoggedIn(),
-        (req, res) => {
-            log.debug('EventsAPI edit ' + _.get(req, 'params.event'));
-        });
+        //Deprecated??? (by '/events/update')
+    // app.put('/events/:event/edit',
+    //     userRole.isLoggedIn(), userRole.isAdmin(),
+    //     (req, res) => {
+    //         log.debug('EventsAPI edit ' + _.get(req, 'params.event'));
+    //     });
 
 }
