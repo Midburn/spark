47aa6ff554a42e26d7848dea13404dc5f6f9d234
Artyom Katsap <artyom.katsap@gmail.com>
2018-01-04 20:05:08 +0200

    added setting manager as main_contact in camps

--- scripts/update_art_managers_roles.js
+++ scripts/update_art_managers_roles.js
@@ -1,3 +1,4 @@
+
 let knex = require('../libs/db').knex;
 
 async function updateRoles() {
@@ -19,15 +20,32 @@ async function updateRoles() {
             }
             let user = users[0];
             console.log(user);
-             
+            let manager_user_id = user.user_id; 
             let user_roles = user.roles.split(',').filter((x) => x);
             if (!user_roles.includes('camp_manager')) {
                 user_roles.push('camp_manager');
                 await knex('users').where('email', manager.email).update('roles', user_roles.join(','));
 
             }
+            // set as main contact
+            let found_camps = await knex('camps').select('id', 'main_contact').where('id', manager.camp_id);
+            if (found_camps.length === 0) {
+                console.log('Could not camp ' + manager.email)
+                return;
+            }
+            let camp = found_camps[0];
+            if (!camp.main_contact) {
+                //set main contact
+                console.log('Setting camp ' + manager.camp_id + ' main contact to ' + manager_user_id + ' ... ')                
+                await knex('camps').where('id', manager.camp_id).update('main_contact', manager_user_id);
+                console.log('Success!!!')
+            }
+            else {
+                console.log('Camp ' + manager.camp_id + ' already has a main contact defined')
+            }
         }
         catch (e) {
+            console.log('error')
             console.log(e)
         }
     }));
