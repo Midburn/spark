a27515041a2ed4ae7c9783f089f537c3dad8b782
Ori Hoch <ori@uumpa.com>
2018-01-06 16:39:05 +0200

    update continuous deployment for production

--- .travis/continuous_deployment.sh
+++ .travis/continuous_deployment.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env bash
 
-if [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] &&\
-   [ "${TRAVIS_BRANCH}" == "${DEPLOY_BRANCH}" ] &&\
+if [ "${DEPLOY_ENVIRONMENT}" != "" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ] && [ "${TRAVIS_BRANCH}" == "${DEPLOY_BRANCH}" ] &&\
    [ "${TRAVIS_COMMIT_MESSAGE}" != "" ] && ! echo "${TRAVIS_COMMIT_MESSAGE}" | grep -- --no-deploy && [ "${TRAVIS_COMMIT}" != "" ]
 then
-    openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in ./midburn-k8s-ops.json.enc -out ./secret-midburn-k8s-ops.json -d
+    openssl aes-256-cbc -K $encrypted_f2bd2a0d33d6_key -iv $encrypted_f2bd2a0d33d6_iv -in ./secret-k8s-ops.json.enc -out secret-k8s-ops.json -d
+    K8S_ENVIRONMENT_NAME="${DEPLOY_ENVIRONMENT}"
     OPS_REPO_SLUG="Midburn/midburn-k8s"
     OPS_REPO_BRANCH="master"
-    IMAGE_TAG="gcr.io/uumpa123/spark:${TRAVIS_COMMIT}"
+    IMAGE_TAG="gcr.io/midbarrn/midburn-spark-cd:${TRAVIS_COMMIT}"
     B64_UPDATE_VALUES=`echo '{"spark":{"image":"'${IMAGE_TAG}'"}}' | base64 -w0`
     HELM_UPDATE_COMMIT_MESSAGE="${DEPLOY_ENVIRONMENT} spark image update --no-deploy"
     wget https://raw.githubusercontent.com/OriHoch/sk8s-ops/master/run_docker_ops.sh
-    chmod +x run_docker_ops.sh
-    ! ./run_docker_ops.sh "${DEPLOY_ENVIRONMENT}" "
-        ! ./helm_update_values.sh '${B64_UPDATE_VALUES}' '${HELM_UPDATE_COMMIT_MESSAGE}' '${K8S_OPS_GITHUB_REPO_TOKEN}' '${OPS_REPO_SLUG}' '${OPS_REPO_BRANCH}' \
-            && echo 'failed helm update values' && exit 1;
-        ! kubectl set image deployment/spark spark=${IMAGE_TAG} && echo 'failed to patch deployment' && exit 1;
+    ! charts/sk8s-ops/run_docker_ops.sh "${K8S_ENVIRONMENT_NAME}" "
+       RES=0;
+       ! ./helm_update_values.sh '${B64_UPDATE_VALUES}' '${HELM_UPDATE_COMMIT_MESSAGE}' '${K8S_OPS_GITHUB_REPO_TOKEN}' '${OPS_REPO_SLUG}' '${OPS_REPO_BRANCH}' \
+            && echo 'failed helm update values' && RES=1;
+        ! kubectl set image deployment/spark spark=${IMAGE_TAG} && echo 'failed to patch deployment' && RES=1;
         cd /spark;
         ! gcloud container builds submit --tag $IMAGE_TAG . \
-            && echo 'failed to build spark image' && exit 1;
-        exit 0
-      " "orihoch/sk8sops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" "./secret-midburn-k8s-ops.json" "-v `pwd`:/spark" \
+            && echo 'failed to build spark image' && RES=1;
+        [ '${TRAVIS_TAG}' != '' ] && gcloud docker -- tag $IMAGE_TAG 'gcr.io/midbarrn/midburn-spark-tag:${TRAVIS_TAG}' \
+            && gcloud docker -- push 'gcr.io/midbarrn/midburn-spark-tag:${TRAVIS_TAG}'
+        exit $RES
+    " "orihoch/sk8s-ops" "${OPS_REPO_SLUG}" "${OPS_REPO_BRANCH}" \
         && echo 'failed to run docker ops' && exit 1
 fi
 
