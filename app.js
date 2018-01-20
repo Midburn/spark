5d301b22ffff25ea745494189babbd4c5d2eeab6
Elad Lachmi <elad.lachmi@gmail.com>
2018-01-20 13:06:08 +0200

    Downgraded node to 7 until node-sass issue is resolved by webpack. Removed last traces of bower and updated ependaancies.

--- app.js
+++ app.js
@@ -57,6 +57,7 @@ app.use(compileSass({
     logToConsole: false // If true, will log to console.error on errors
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
 
 app.use(function (req, res, next) {
     res.locals.req = req;
