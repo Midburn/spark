f61844c81b6fc276b630ff055cea7c39c8c08621
Yaniv Peretz <yip528@gmail.com>
2018-02-04 14:11:13 +0200

    Event form - control dates
    adds control dates to the event form.
    * ticket allocation - ticket allocation is possible only within the dates period for admin to allocation allowance to the camps, and for the camps to allocate to members.
    * camps last edit date - if populated campAdmin could edit the camp only until that date.

--- routes/events_routes.js
+++ routes/events_routes.js
@@ -1,54 +1,29 @@
 const userRole = require('../libs/user_role');
 const Event = require('../models/event').Event;
-const _ = require('lodash')
-
+    
 module.exports = function (app, passport) {
 
-    app.get('/:lng/events-admin', userRole.isLoggedIn(), (req, res) => {
+    app.get('/:lng/events-admin', userRole.isAdmin(), (req, res) => {
         // res.send('Events Index')
         res.render('pages/events/index', {
-            t_prefix: 'events:'
+            t_prefix: 'events:',
+            event: {},
         })
     });
 
-    // new camp
-    // need to be changed to /:lng/events-admin/edit
-    // creating a new event is availble directly form the UI- green button
-    app.get('/:lng/events-admin/new', userRole.isAdmin(), (req, res) => {
-
-        req.event = {};
-        res.render('pages/events/edit', {
-            event: req.event,
-            user: req.user,
-            isNew: true
-        });
-    });
-
     // Read
-    app.get('/:lng/events-admin/:id', userRole.isLoggedIn(), (req, res) => {
-        Event.where({event_id: req.params.id}).fetch()
-            .then((event) => {
-                
-                req.event = {
-                    'event_id': req.params.id,
-                    'ext_id_event_id': _.get(event.attributes, 'ext_id_event_id'),
-                    'name': _.get(event.attributes, 'name'),
-                    'gate_code': _.get(event.attributes, 'gate_code'),
-                    'gate_status': _.get(event.attributes, 'gate_status'),
-                    
-                    'addinfo_json': JSON.parse(_.get(event.attributes, 'addinfo_json'))
-                };
-                res.render('pages/events/edit', {
-                    event: req.event,
-                    user: req.user,
-                });
-                    
+    app.get('/:lng/events-admin/:id', userRole.isAdmin(), (req, res) => {
+        Event.where({ event_id: req.params.id }).fetch()
+            .then(json => {
+                return json.attributes
+            }).then(event => {
+                event.addinfo_json = JSON.parse(event.addinfo_json);
+                const data = {
+                    t_prefix: 'events:',
+                    'event': event,
+                }
+                res.render('pages/events/edit', data);
             })
     });
 
-    // Edit
-    app.get('/:lng/events-admin/:id/edit', userRole.isLoggedIn(), (req, res) => {
-        var event_id = req.params.id;
-        res.send('Edit event ' + event_id)
-    });
 };
