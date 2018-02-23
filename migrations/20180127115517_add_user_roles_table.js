db1e5143a3f7d4a377acea9236f91325c2b47680
Artyom Katsap <artyom.katsap@gmail.com>
2018-02-23 17:54:27 +0200

    removed camp_id foreign key

--- migrations/20180127115517_add_user_roles_table.js
+++ migrations/20180127115517_add_user_roles_table.js
@@ -3,12 +3,11 @@ const constants = require('../models/constants');
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema.createTable(constants.USER_ROLES_TABLE_NAME, (table) => {
+            table.increments('id').primary();
             table.integer('camp_id').unsigned();
             table.integer('user_id').unsigned();
             table.string('event_id',32);
             table.string('role',100);
-            table.unique(['camp_id', 'user_id', 'event_id']);
-            table.foreign('camp_id').references('id').inTable(constants.CAMPS_TABLE_NAME);
             table.foreign('user_id').references('user_id').inTable(constants.USERS_TABLE_NAME);
             table.foreign('event_id').references('event_id').inTable(constants.EVENTS_TABLE_NAME);
         })
