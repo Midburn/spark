82726f690d05aa0ea11653a727587cfd0be72bcf
Nitay Rabinovich <nitayrabi@gmail.com>
2018-02-25 18:33:50 +0200

    Upgrade seed (#648)
    * Create static admin data
    Add events (2017)
    
    * Hardcoded `a` password for all mock users.
    User `1` is always `a` (admin)
    Validate last id (for PK mismatch)
    Create `camp_members` table entries.
    
    * Add users - a, b, c
    Add created-at field for events.
    Use more camps and less users
    
    * Delete redundant admin.js file
    
    * Add static camps for each year for each static user (a, b, c)
    static status for static camps
    
    * Validate seed.
    
    * Remove useless escape char
    
    * Create unique names for static camps
    Only add static camps if random is chosen (to prevent dups)
    
    * Add random `__prototype` to static camps

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -5,6 +5,7 @@ const dropDb = require('./0_drops');
 const addUsersToDb = require('./1_users');
 const addEventsToDb = require('./2_event');
 const addCampsToDb = require('./3_camps');
+const addCampMembers = require('./4_campMembers');
 const MOCK_USERS_SCHEMA = require('./gen/mockSchema/users.schema');
 const MOCK_EVENTS_SCHEMA = require('./gen/mockSchema/events.schema');
 const MOCK_CAMPS_SCHEMA = require('./gen/mockSchema/camps.schema');
@@ -14,16 +15,10 @@ const replaceStatic = process.argv.includes('replace');
 const nosave = process.argv.includes('nosave');
 const keepdb = process.argv.includes('keepdb');
 const scale = Number(process.argv.pop());
+const constants = require('../models/constants');
 
-if (random) {
-    log.info('Generating random data...');
-    if (!scale) {
-        console.log('When generating random data you must state scale number as last parameter');
-        process.exit(1);
-    }
-}
 if (replaceStatic) {
-    log.info('Replacing static data...');
+
 }
 if (nosave) {
     log.info('Running without saving to db');
@@ -32,6 +27,60 @@ if (nosave) {
 if (scale > 40) {
     log.info('Chosen scale to big... try a smaller one');
 }
+
+const initStaticCamps = (camps, events) => {
+    function getCampDiscription(id, lang) {
+        switch (id) {
+            case 1:
+                return lang === 'en' ? 'Admin`s Camp' : 'מחנה אדמין!';
+            case 2:
+                return lang === 'en' ? 'Camp Managers`s Camp' : 'מחנה מנהל b';
+            case 3:
+                return lang === 'en' ? 'Normal User`s Camp' : 'מחנה של משתמש רגיל';
+        }
+    }
+    /**
+     * Create static camps in each events for each static user (a, b, c)
+     * Unique is used to prevent unique name dups.
+     */
+    let unique = 0;
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    for (const event of events) {
+        for (let i = 1; i <= 3; i++) {
+            const newCamp = {
+                ...camps[camps.length - 1],
+                event_id: event.event_id,
+                status: 'open',
+                main_contact: i,
+                moop_contact: i,
+                safety_contact: i,
+                contact_person_id: i,
+                camp_name_he: getCampDiscription(i) + ' ' + event.event_id + ` ${unique}`,
+                camp_name_en: getCampDiscription(i, 'en') + ' ' + event.event_id + ` ${unique}`,
+                camp_desc_he: getCampDiscription(i) + ' ' + event.event_id + ` ${unique}`,
+                camp_desc_en: getCampDiscription(i, 'en') + ' ' + event.event_id + ` ${unique}`,
+                __prototype: constants.CAMP_PROTOTYPE[getRandomInt(0, 2)]
+            };
+            newCamp.id = camps.push(newCamp);
+            unique++;
+        }
+    }
+    return camps;
+};
+
+const correlateData = (users, camps) => {
+    const campMembers = [];
+    for (const camp of camps) {
+        const campAdmin = users.find(user => user.user_id === camp.contact_person_id);
+        if (campAdmin) {
+            campMembers.push({camp_id: camp.id, user_id: campAdmin.user_id, status: 'approved_mgr'});
+        }
+    }
+    return campMembers;
+};
+
 const seed = async (scale = 1) => {
     try {
         if (!keepdb) {
@@ -39,6 +88,11 @@ const seed = async (scale = 1) => {
         }
         let mockData;
         if (random) {
+            log.info('Generating random data...');
+            if (!scale) {
+                console.log('When generating random data you must state scale number as last parameter');
+                process.exit(1);
+            }
             // Create some data for injection
             mockData = await generate(scale);
         } else {
@@ -50,22 +104,30 @@ const seed = async (scale = 1) => {
         }
         const users = mockData[MOCK_USERS_SCHEMA.NAME];
         const events = mockData[MOCK_EVENTS_SCHEMA.NAME];
-        const camps = mockData[MOCK_CAMPS_SCHEMA.NAME];
-        if (!nosave) {
-            await addUsersToDb(users);
-            await addEventsToDb(events);
-            await addCampsToDb(camps);
+        let camps = mockData[MOCK_CAMPS_SCHEMA.NAME];
+        if (random) {
+            camps = initStaticCamps(camps, events);
         }
+        // Create link between camps and users
+        const campMembers = correlateData(users, camps);
         if (replaceStatic) {
+            log.info('Replacing static data...');
             utils.saveFile(MOCK_USERS_SCHEMA.NAME, users);
             utils.saveFile(MOCK_EVENTS_SCHEMA.NAME, events);
             utils.saveFile(MOCK_CAMPS_SCHEMA.NAME, camps);
+        }
+        if (!nosave) {
+            await addEventsToDb(events);
+            await addUsersToDb(users);
+            await addCampsToDb(camps);
+            await addCampMembers(campMembers);
         }
         log.info(`Seeding process done, seeded ${users.length} users, ${camps.length} camps and ${events.length} events`);
         process.exit(0);
         return mockData;
     } catch (err) {
         log.error(`An error occurred while seeding project - ${err}`);
+        process.exit(-1);
     }
 
 };
