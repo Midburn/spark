a6b45f6b082d15d0c9876e2a14815a6d175cbb68
nitayrabi <nitayrabi@gmail.com>
2018-02-27 00:27:13 +0200

    Fix 404 redirect

--- app.js
+++ app.js
@@ -162,11 +162,11 @@ mail.setup(app);
 
 /** #####################
  *      Mapping Routes
-    ##################### */ 
+    ##################### */
 // Mapping all Api routes
-require('./routes/index.js').api(app, passport)
+require('./routes/index.js').api(app, passport);
 // Maping all page routes
-app.use("/", require('./routes/index.js').app)
+app.use("/", require('./routes/index.js').app);
 
 // Recaptcha setup with siteId & secret
 recaptcha.init(recaptchaConfig.sitekey, recaptchaConfig.secretkey);
