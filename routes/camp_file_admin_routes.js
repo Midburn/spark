bae1cce504498d2092fd027de8c63f82ffe453d6
Elad Lachmi <elad.lachmi@gmail.com>
2018-02-28 08:58:52 +0200

    [WIP] Admin page for files uploaded by camp managers

--- routes/camp_file_admin_routes.js
+++ routes/camp_file_admin_routes.js
@@ -1,6 +1,10 @@
 const userRole = require('../libs/user_role'),
 express = require('express'),
-router = express.Router()
+router = express.Router(),
+LOG = require('../libs/logger')(module),
+S3 = require('../libs/aws-s3'),
+config = require('config'),
+awsConfig = config.get('aws_config')
 // Camp = require('../models/camp').Camp
 
 router.get('/', userRole.isLoggedIn(), userRole.isAdmin(), (req, res) => {
@@ -10,6 +14,24 @@ router.get('/', userRole.isLoggedIn(), userRole.isAdmin(), (req, res) => {
     }])
 
     res.render('pages/camp_files')
+})
+
+router.post('/', userRole.isLoggedIn(), userRole.isAdmin(), async (req, res) => {
+    const prefix = req.body.prefix,
+        s3Client = new S3()
+
+    try {
+        let objectList = await s3Client.listBucket(awsConfig.buckets.camp_file_upload, prefix)
+        return res.status(200).json({
+            list: objectList
+        })
+    } catch (err) {
+        LOG.error(err.message)
+        return res.status(500).json({
+            error: true,
+            message: 'Error listing objects in bucket!'
+        })
+    }
 })
 
 module.exports = router
