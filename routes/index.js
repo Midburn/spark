f2c06287ca29c7c9f0a2b739b186239bf0112806
nitayr <Nitay.Rabinovich@Cyberbit.com>
2018-03-24 12:41:29 +0300

    Merge fixs

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,8 @@ app.use("/:lng/npo", require("./pages/npo_routes"));
 app.use("/:lng/npo-admin", require("./pages/npo_admin_routes"));
 app.use("/:lng/gate", require("./pages/gate_routes"));
 app.use("/:lng/volunteering", require("./pages/volunteering_routes"));
-//TODO: refactor camps routes
 app.use("/:lng/", require('./pages/suppliers_routes'));
+//TODO: refactor camps routes
 app.use("/:lng/", require("./pages/camps_routes"));
 // Suppliers
 
