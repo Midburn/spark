4f932f0252022db09466500d36dbd886e2071796
Pavel Suchman <pavel.suchman@gmail.com>
2018-03-30 12:57:01 +0300

    Drop unneeded column

--- migrations/20180327173047_add_supplier_contracts.js
+++ migrations/20180327173047_add_supplier_contracts.js
@@ -5,10 +5,8 @@ exports.up = function(knex, Promise) {
         // Suppliers table
         knex.schema.createTable(constants.SUPPLIERS_CONTRACTS_TABLE_NAME, function(table) {
             table.timestamps();
-
             table.integer('supplier_id', 9).unsigned().primary();
             table.string('contract_name', 100);
-            table.string('contract_path', 100);
         }),
     ]);
 };
