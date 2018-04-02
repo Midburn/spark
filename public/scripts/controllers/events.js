7235281641017ab131d0908c7a8ee9e987782328
Yuri Prezument <y@yprez.com>
2018-04-02 20:38:03 +0300

    Events table - take out date fields from addinfo_json to seperate columns (#805)
    * Events table - created_at as seperate datetime field
    
    * Add migration
    * Template
    * Update seeds / fixtures
    * Routes (saving fields)
    * Default event values in controller
    
    Ref #723
    
    * Data migration for events.created_at
    
    Ref #723
    
    * Take out start_date, end_date out of event.addinfo_json
    
    * Update migration, route, fronend, etc.
    * In the same way created_at column was added

--- public/scripts/controllers/events.js
+++ public/scripts/controllers/events.js
@@ -27,8 +27,8 @@ events_app.controller("eventsController", ($scope, $http, $filter) => {
 events_app.controller("eventsFormController", ($scope, $http, $filter) => {
     //initiate a new event, or fetch evet details for edit.
     $scope.isNew = isNew;
-    $scope.event = isNew ? { addinfo_json: { created_at: new Date() } } : editEvent;
-    $scope.eventStarted = $scope.event.addinfo_json.start_date < new Date();
+    $scope.event = isNew ? {created_at: new Date(), addinfo_json: {}} : editEvent;
+    $scope.eventStarted = $scope.event.start_date < new Date();
 
     $scope.createEvent = () => {
         let _url = '/events/new';
