85346aed96ddfde9d98860c99d68c17acd4562e1
Ben Marder <benb@razorgrip.com>
2018-04-03 01:41:54 +0300

    Unnecessary code removed. Suppliers routes now use suppliers api to fetch data. In new supplier wizard: category field changed to a drop down that contain 'Moving' and 'Other'. 'Breadcrumbs doesn't show on edit page' bug fixed

--- public/scripts/controllers/supplier_edit.js
+++ public/scripts/controllers/supplier_edit.js
@@ -7,92 +7,18 @@ var angular_getCamps = function ($http, $scope, supplier_id) {
     });
   }
 }
-var angular_updateUser = function ($http, $scope, action_type, user_rec) {
-    var camp_id = user_rec.camp_id;
-    var user_name = user_rec.user_name;
-    var user_id = user_rec.user_id;
-    var lang = $scope.lang;
-    if (lang === undefined) {
-        lang = 'he';
-    }
-    var tpl, action_tpl;
-
-    if (lang === "he") {
-        // debugger;
-        action_tpl = {
-            approve: 'לאשר את',
-            delete: 'למחוק את',
-            reject: 'לדחות את',
-            approve_mgr: 'להפוך למנהל את',
-            remove: 'להסיר את',
-            pre_sale_ticket : 'לאשר כרטיס מוקדם',
-        };
-        tpl = {
-            alert_title: "האם את/ה בטוח?",
-            alert_text: "האם את/ה בטוח שתרצה " + action_tpl[action_type] + " משתמש " + user_name + "?",
-            alert_success_1: action_type + "!",
-            alert_success_2: "משתמש " + user_name + action_type,
-            alert_success_3: " בהצלחה",
-        };
-    } else {
-        action_tpl = {
-            approve: 'Approve',
-            delete: 'Delete',
-            reject: 'Reject',
-            approve_mgr: 'Set Manager',
-            remove: 'Remove',
-            pre_sale_ticket: 'Update Pre Sale Ticket',
-        };
-        tpl = {
-            alert_title: "Are you sure?",
-            alert_text: "Are you sure you would like to " + action_tpl[action_type] + " " + user_name + "?",
-            alert_success_1: action_type + "!",
-            alert_success_2: user_name + "has been " + action_type,
-            alert_success_3: "success",
-        };
-    }
-
-    sweetAlert({
-        title: tpl.alert_title,
-        text: tpl.alert_text,
-        type: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#DD6B55",
-        confirmButtonText: "Yes",
-        closeOnConfirm: false
-    },
-        () => {
-            var request_str = `/camps/${camp_id}/members/${user_id}/${action_type}`
-            $http.get(request_str).then((res) => {
-                sweetAlert(tpl.alert_success_1, tpl.alert_success_1, "success");
-                $scope.getCamps(camp_id);
-            }).catch((err) => {
-                jsonError=err.data.data.message;
-                sweetAlert("Error!", "Something went wrong, please try again later \n" + jsonError, "error");
-            })
-        });
-}
 
 suppliers_app.controller("supllierEditController", ($scope, $http, $filter) => {
     var supplier_id = document.querySelector('#meta__supplier_id').value;
-    var lang = $scope.lang;
-    if (lang === undefined) {
-        lang = 'he';
-    }
+    var lang = document.getElementById('meta__lang').value || 'he';  
     if (lang === "he") {
         $scope.status_options = [
-            { id: 'food', value: 'אוכל' },
-            { id: 'water', value: 'מים' },
-            { id: 'shade', value: 'צל' },
             { id: 'carriage', value: 'הובלה' },
             { id: 'other', value: 'אחר' }]
     } else {
         $scope.status_options = [
-            { id: 'food', value: 'food' },
-            { id: 'water', value: 'water' },
-            { id: 'shade', value: 'shade' },
-            { id: 'carriage', value: 'carriage' },
-            { id: 'other', value: 'other' }]
+            { id: 'carriage', value: 'Carriage' },
+            { id: 'other', value: 'Other' }]
     }
     $http.get(`/camps_all`).then((res) => {
         $scope.allCamps = res.data.camps;
@@ -101,7 +27,6 @@ suppliers_app.controller("supllierEditController", ($scope, $http, $filter) => {
         $http.delete(`/suppliers/${supplier_id}/camps/${campId}`).then((res) => {
          //TODO check if the user can delete the camp
         });
-
         $scope.getCamps();
     }
     $scope.getCamps = () => {
@@ -117,35 +42,24 @@ suppliers_app.controller("supllierEditController", ($scope, $http, $filter) => {
         $scope.current_supplier_id = supplier_id;
         $scope.getCamps();
     }
-    $scope.lang = document.getElementById('meta__lang').value;
-    // $scope.grouptype = document.getElementById('meta__grouptype').value;
+    
     $scope.addCamp= () => {
         var supplier_id = $scope.current_supplier_id;
         const camp_id = $scope.add_camp_id;
         $http.put(`/suppliers/${supplier_id}/camps/${camp_id}`)
-            .then(function (res) {
+             .then(function (res) {
                 // update table with new data
                 $scope.getCamps();
                 $scope.add_camp_id = '';
-        }).catch((err) => {
-            if (err.data.data.message.indexOf("Duplicate entry")) {
-                sweetAlert("!oops","You are trying to add a camp that is already exist","warning");
-            }
-            else {
-                sweetAlert("Error!", "Add new camp error: " + err.data.data.message, "error");
-            }
+             }).catch((err) => {
+                if (err.data.data.message.indexOf("Duplicate entry")) {
+                    sweetAlert("!oops","You are trying to add a camp that is already exist","warning");
+                }
+                else {
+                    sweetAlert("Error!", "Add new camp error: " + err.data.data.message, "error");
+                }
         });
     }
-    $scope.updateUser = (user_name, user_id,action_type) => {
-        var camp_id = $scope.current_camp_id;
-        var user_rec = {
-            camp_id: camp_id,
-            user_name: user_name,
-            user_id: user_id,
-        }
-        angular_updateUser($http, $scope, action_type, user_rec);
-    }
-
 }); //end of controller
 
 suppliers_app.controller("homeController", ($scope, $http, $filter) => {
--- routes/pages/suppliers_routes.js
+++ routes/pages/suppliers_routes.js
@@ -1,130 +1,40 @@
 const userRole = require('../../libs/user_role');
-const Supplier = require('../../models/suppliers').Suppliers;
 const Event = require('../../models/event').Event;
 const express = require('express');
+const superagent = require('superagent');
+
 const router = express.Router({
     mergeParams: true
 });
 
 const __supplier_data_to_json = function (supplier) {
-    let supplier_data = supplier.toJSON();
     let supplier_check_null = [
         'updated_at', 'supplier_id', 'supplier_name_en', 'supplier_name_he', 'main_contact_name',
         'main_contact_position', 'main_contact_phone_number', 'supplier_category', 'supplier_website_link',
         'supplier_midmarket_link', 'comments','created_at'];
     for (let i in supplier_check_null) {
-        if (supplier_data[supplier_check_null[i]] === null) {
-            supplier_data[supplier_check_null[i]] = '';
+        if (supplier[supplier_check_null[i]] === null) {
+            supplier[supplier_check_null[i]] = '';
         }
     }
-    return supplier_data;
-};
-const __render_supplier = function (supplier, req, res) {
-    let supplier_id;
-    if (['int', 'string'].indexOf(typeof supplier) > -1) {
-        supplier_id = parseInt(supplier);
-    } else {
-        supplier_id = camp.id;
-    }
-    Supplier.forge({
-        supplier_id: supplier_id,
-      //  event_id: req.user.currentEventId
-    }).fetch({}).then((supplier) => {
-           // supplier.init_t(req.t);
-
-            // if user is camp_member, we can show all
-            // let _user = camp.isUserCampMember(req.user.id);
-            let supplier_data = __supplier_data_to_json(supplier);
-            let data = {
-                user: req.user,
-                userLoggedIn: req.user.hasRole('logged in'),
-                id: supplier_id,
-                camp: supplier_data,
-                breadcrumbs: req.breadcrumbs(),
-                details: supplier_data,
-                isAdmin: req.user.isAdmin,
-                // isUserCampMember: (supplier.isUserCampMember(req.user.id) || req.user.isAdmin),
-                // isUserInCamp: (supplier.isUserInCamp(req.user.id) || req.user.isAdmin),
-                // isCampManager: (supplier.isCampManager(req.user.id) || req.user.isAdmin),
-                // main_contact: supplier.isUserInCamp(supplier.attributes.main_contact),
-                // moop_contact: supplier.isUserInCamp(supplier.attributes.moop_contact),
-                // safety_contact: supplier.isUserInCamp(supplier.attributes.safety_contact),
-            };
-            Event.get_event_controllDates(req.user.currentEventId).then(controllDates => {
-                controllDates = controllDates || {};
-                data.supplierslastEditDate = controllDates.edit_suppliers_lastDate || new Date(Date.now() + 1000*60*60*24*30);
-                res.render('pages/suppliers/camp', data);
-            })
-    }).catch((e) => {
-        res.status(500).json({
-            error: true,
-            data: {
-                message: e.message
-            }
-        });
-    });
-};
-// ==============
-// suppliers Routing
-// ==============
-// suppliers index page, create new camp
-router.get('/suppliers', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs([{
-        name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
-    },
-    {
-        name: 'suppliers:breadcrumbs.join_camp',
-        url: '/' + req.params.lng + '/suppliers'
-    }]);
-    Supplier.getSupplierCamps((suppliers) => {
-        if (suppliers.length === 0 || !req.user.attributes.camp || !req.user.attributes.camp_manager) {
-            // if (req.user.attributes.suppliers.length === 0 || !req.user.attributes.camp_manager) {
-            const camp = req.user.attributes.camp;
-            res.render('pages/suppliers/index_user', {
-                user: req.user,
-                camp: camp,
-                breadcrumbs: req.breadcrumbs()
-            });
-        } else {
-            __render_supplier(req.user.attributes.camp, req, res);
-        }
-    }, req.t);
-});
-// Read
-router.get('/suppliers/:id(\\d+)/', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs([{
-        name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
-    },
-    {
-        name: 'suppliers:breadcrumbs.my_camp',
-        url: '/' + req.params.lng + '/suppliers'
-    },
-    {
-        name: 'suppliers:breadcrumbs.camp_stat',
-        url: '/' + req.params.lng + '/suppliers/' + req.params.id
-    }]);
-    __render_supplier(req.params.id, req, res);
-});
+    return supplier;
+ };
 
 // new suppliers
 router.get('/suppliers/new', userRole.isAdmin(), (req, res) => {
+    const lang = req.params.lng || 'he';
     req.breadcrumbs([{
         name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
+        url: '/' + lang + '/home'
     },
     {
         name: 'suppliers:breadcrumbs.manage',
-        url: '/' + req.params.lng + '/suppliers-admin'
+        url: '/' + lang + '/suppliers-admin'
     },
     {
         name: 'suppliers:breadcrumbs.new',
-        url: '/' + req.params.lng + '/suppliers/new/?c=' + req.query.c
+        url: '/' + lang + '/suppliers/new/?c=' + req.query.c
     }]);
-
-    // let prototype = constants.prototype_camps.THEME_CAMP.id;
-    // let result = Camp.prototype.__parsePrototype(prototype, req.user);
     let controllDates = {
         appreciation_tickets_allocation_start: null,
         appreciation_tickets_allocation_end: null
@@ -136,97 +46,51 @@ router.get('/suppliers/new', userRole.isAdmin(), (req, res) => {
         details: {},
         isAdmin: req.user.isAdmin,
         isNew: true,
-      //  __groups_prototype: prototype,
-        t_prefix: 'suppliers:',//
-        //isArt: prototype === constants.prototype_suppliers.ART_INSTALLATION.id,
-        //isCamp: prototype === constants.prototype_suppliers.THEME_CAMP.id,
-        //isProd: prototype === constants.prototype_suppliers.PROD_DEP.id,
-        controllDates: controllDates
+        t_prefix: 'suppliers:',
+        controllDates: controllDates,
+        language: lang
     });
 });
 // Edit
 router.get('/suppliers/:id/edit', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs([{
-        name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
-    },
-    {
-        name: 'suppliers:breadcrumbs.manage',
-        url: '/' + req.params.lng + '/suppliers-admin'
-    },
-    {
-        name: 'suppliers:breadcrumbs.edit',
-        url: '/' + req.params.lng + '/suppliers/' + req.params.id + '/edit'
-    }]);
-    Supplier.forge({
-        supplier_id: req.params.id,
-    }).fetch({
-       // withRelated: [constants.SUPPLIERS_RELATIONS_TABLE_NAME]
-    }).then((supplier) => {
-        supplier.getSupplierCamps((camps) => {
-            let supplier_data = __supplier_data_to_json(supplier);
-            let _edit_rec = {
+    const supplier_id = req.params.id;
+    const lang = req.params.lng || 'he';
+    let supplier;
+    superagent
+        .get(`${process.env.SPARK_SERVER_URL}/suppliers/${supplier_id}`)
+        .then((response) => {
+            req.breadcrumbs([{
+                name: 'breadcrumbs.home',
+                url: '/' + lang + '/home'
+            },
+            {
+                name: 'suppliers:breadcrumbs.manage',
+                url: '/' + lang + '/suppliers-admin'
+            },
+            {
+                name: 'suppliers:breadcrumbs.edit',
+                url: '/' + lang + '/suppliers/' + supplier_id + '/edit'
+            }]);
+            supplier = JSON.parse(response.text) || {}
+            const currentEventID = req.session.passport.user.currentEventId;
+            const supplier_data = __supplier_data_to_json(supplier.supplier);
+            const params = {
                 user: req.user,
                 breadcrumbs: req.breadcrumbs(),
                 supplier: supplier_data,
-                camps: camps,
-                details:supplier_data,
+                details: {},
                 isNew: false,
                 isAdmin: req.user.isAdmin,
-                __groups_prototype: supplier.attributes.__prototype,
                 t_prefix: "suppliers:",
-             //   isArt: supplier.attributes.__prototype === constants.prototype_suppliers.ART_INSTALLATION.id,
-             //   isCamp: supplier.attributes.__prototype === constants.prototype_suppliers.THEME_CAMP.id,
-             //   isProd: supplier.attributes.__prototype === constants.prototype_suppliers.PROD_DEP.id,
-            };
-            const currentEventID = req.session.passport.user.currentEventId;
-            Event.get_event_controllDates(currentEventID)
-            .then(controllDates => {
-                _edit_rec.controllDates = controllDates || {};
-                res.render('pages/suppliers/edit',_edit_rec);
-                });
+                language: lang
+            };   
+            res.render('pages/suppliers/edit',params);    
+        }).catch((err) => {
+            console.log(`get(${process.env.SPARK_SERVER_URL}/suppliers/${supplier_id}) error:`,err);
+            res.render('pages/error/edit',{errorMessage: err.message,error: err});  
+        });
+});
 
-        },req,req.params.id);
-    })
-});
-// suppliers statistics
-router.get('/suppliers-stats', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs([{
-        name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
-    },
-    {
-        name: 'suppliers:breadcrumbs.manage',
-        url: '/' + req.params.lng + '/suppliers-admin'
-    },
-    {
-        name: 'suppliers:breadcrumbs.stats',
-        url: '/' + req.params.lng + '/suppliers-stats'
-    }]);
-    res.render('pages/suppliers/stats', {
-        user: req.user,
-        breadcrumbs: req.breadcrumbs()
-    });
-});
-// suppliers members board
-router.get('/suppliers-members', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs([{
-        name: 'breadcrumbs.home',
-        url: '/' + req.params.lng + '/home'
-    },
-    {
-        name: 'suppliers:breadcrumbs.manage',
-        url: '/' + req.params.lng + '/suppliers-admin'
-    },
-    {
-        name: 'suppliers:breadcrumbs.members',
-        url: '/' + req.params.lng + '/suppliers-members'
-    }]);
-    res.render('pages/suppliers/members', {
-        user: req.user,
-        breadcrumbs: req.breadcrumbs()
-    });
-});
 // suppliers documents
 router.get('/suppliers-docs', userRole.isLoggedIn(), (req, res) => {
     req.breadcrumbs([{
@@ -278,14 +142,6 @@ router.get('/suppliers-admin/:cardId*?', userRole.isLoggedIn(), (req, res) => {
             });
         }
 
-});
-// Program
-router.get('/program', userRole.isLoggedIn(), (req, res) => {
-    req.breadcrumbs('suppliers-new_program');
-    res.render('pages/suppliers/program', {
-        user: req.user,
-        camp_name_en: req.query.c
-    });
 });
 
 module.exports = router;
