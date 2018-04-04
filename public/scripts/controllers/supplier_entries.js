30d5c71083928ab576796b286503cdcc83bc84c2
nitayrabi <nitayrabi@gmail.com>
2018-04-04 23:08:36 +0300

    Add functionaly to view entries Add functionality to add/exit entry validate data on adding entry

--- public/scripts/controllers/supplier_entries.js
+++ public/scripts/controllers/supplier_entries.js
@@ -1,37 +1,27 @@
-var suplliers_all;
-var groups_prototype;
 
 suppliers_app.controller("supplierEntriesController", function ($scope, $http, $filter) {
-    getAllSuppliers = function (on_success) {
-        if (suplliers_all) {
-            on_success(suplliers_all);
-        } else {
-            const url = '/suppliers';
-            $http.get(url)
-                .then((res) => {
-                    suplliers_all = res;
-                    on_success(res);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
-    };
-    getEntriesByStatus = function (status, on_success) {
-        if (suplliers_all) {
-            on_success(suplliers_all);
-        } else {
-            const url = '/suppliers/suppliers_gate_info/' + status;
-            $http.get(url)
-                .then((res) => {
-                    suplliers_all = res;
-                    on_success(res);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }
-    };
+    function getAllSuppliers (on_success) {
+        const url = '/suppliers';
+        $http.get(url)
+            .then((res) => {
+                on_success(res);
+            })
+            .catch(err => {
+                sweetAlert('Could not get supplier list: ' + err.message);s
+            });
+    }
+
+    function getEntriesByStatus (status, on_success) {
+        const url = '/suppliers/suppliers_gate_info/' + status;
+        $http.get(url)
+            .then((res) => {
+                suplliers_all = res;
+                on_success(res);
+            })
+            .catch(err => {
+                sweetAlert('Could not get entries: ' + err.message);
+            });
+    }
 
     // Get suppliers for entry list
     getAllSuppliers((res) => {
@@ -41,11 +31,48 @@ suppliers_app.controller("supplierEntriesController", function ($scope, $http, $
         }, 500)
     });
 
-    getEntriesByStatus('Inside', (res) => {
-        $scope.entries = res.data.suppliers;
-        setTimeout(() => {
-            innerHeightChange()
-        }, 500)
-    });
+    // Get all entries (for 'Inside' status)
+    refreshCurrentEntries();
+
+    // New entry
+    $scope.newEntry = {};
+
+    $scope.addEntry = function () {
+        if (!$scope.suppliers.find(supplier => supplier.supplier_id === $scope.newEntry.supplier_id)) {
+            return sweetAlert(`Unknown supplier, please validate id/vat!`);
+        }
+        if (!$scope.newEntry.supplier_id || !$scope.newEntry.vehicle_plate_number
+            || !$scope.newEntry.number_of_people_entered || !$scope.newEntry.allowed_visa_hours) {
+            return sweetAlert(`Please fill all fields!`);
+        }
+        $http.post(`/suppliers/${$scope.newEntry.supplier_id}/add_gate_record_info/Inside`, $scope.newEntry)
+            .then(done => {
+                sweetAlert(`Supplier entry added!`);
+                $scope.newEntry = {};
+                refreshCurrentEntries();
+            })
+            .catch(err => {
+                sweetAlert(`An error occurred while adding supplier entry ${err.data.data.message}`);
+            });
+    };
+
+    $scope.supplierLeft = function(entry) {
+        $http.post(`/suppliers/${$scope.newEntry.supplier_id}/add_gate_record_info/Outside`, entry)
+            .then(done => {
+                refreshCurrentEntries();
+            })
+            .catch(err => {
+                sweetAlert(`An error occurred while editing supplier entry ${err.data.data.message}`);
+            });
+    };
+
+    function refreshCurrentEntries() {
+        getEntriesByStatus('Inside', (res) => {
+            $scope.entries = res.data.suppliers;
+            setTimeout(() => {
+                innerHeightChange()
+            }, 500)
+        });
+    }
 
 });
