1d90a1d0e1a584b994dd68bb54be96d8306b178e
nitayrabi <nitayrabi@gmail.com>
2018-04-05 13:16:07 +0300

    Add table sorting. Add overdue class (lines are red if supplier is overdue)

--- public/scripts/controllers/supplier_entries.js
+++ public/scripts/controllers/supplier_entries.js
@@ -23,6 +23,10 @@ suppliers_app.controller("supplierEntriesController", function ($scope, $http, $
             });
     }
 
+    // Initial order by last entry
+    $scope.orderEntries = 'enterance_time';
+    $scope.isReverseOrder = true;
+
     // Get suppliers for entry list
     getAllSuppliers((res) => {
         $scope.suppliers = res.data.suppliers;
@@ -64,6 +68,24 @@ suppliers_app.controller("supplierEntriesController", function ($scope, $http, $
             .catch(err => {
                 sweetAlert(`An error occurred while editing supplier entry ${err.data.data.message}`);
             });
+    };
+
+    // Test if entry is past it's exit time;
+    $scope.checkOverdue = function (entry) {
+        const entryTime = moment(entry.enterance_time);
+        const maxExitTime = entryTime.add(entry.allowed_visa_hours, 'hours');
+        const now = new Date();
+        // If maxExitTime is smaller then now - the supplier is overdue.
+        return maxExitTime.toDate().getTime() < now.getTime();
+    };
+
+    $scope.changeOrderBy = function (orderString) {
+        if ($scope.orderEntries === orderString) {
+            // Only toggle ordering
+            $scope.isReverseOrder = !$scope.isReverseOrder;
+            return;
+        }
+        $scope.orderEntries = orderString;
     };
 
     function refreshCurrentEntries() {
