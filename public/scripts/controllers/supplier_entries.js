970e17222960a3c98fa84d6b41e8a2ba8f85391e
Nitay Rabinovich <nitayrabi@gmail.com>
2018-04-09 20:15:51 +0300

    Suppliers gate (#814)
    * Move utility function in api_camps_routes to services.
    Fix linting
    Replace vars with const and let.
    
    * Initial structure (using routers and controllers)
    
    * Refactor api_camp_routes
    Refactor api_events_routes
    
    * Refactor api_routes (auth)
    
    * Refactor public camp routes
    
    * Fix an error in jade file
    Connect new API router to main app.
    
    * Add comments to index
    
    * Fix 404 redirect
    
    * Truly fix 404
    
    * Add S3 changes to new refactored api
    
    * Fix lint
    
    * Merge fixs
    
    * Add DGS date columns to admin edit (GUI only)
    
    * Add DGS column to camps.
    Add DGS column to admin camps management table.
    Add disabled logic (for both quota columns).
    E2E - add dates to event, see the column is not disabled, add dgs tickets.
    
    * Handle dates in event editing
    
    * Add separation in camp edit statistics between ticket types
    Fix counts of statistics (total in camps, allocated, allocated by ticket etc...)
    Add dgs to B.E.
    Validate adding tickets and removing tickets
    
    * Fix counts
    
    * Fix lint
    
    * Initial work on camp edit disabling
    
    * Implement disabling camp-manager page
    
    * Handle no event rules found (fix test)
    
    * continuous deployment from api-refactor-feb-2018 to sparktesting environment (#770)
    
    * Merge DGS changes
    
    * Merge suppliers
    
    * Fix Lint
    Fix Merge
    
    * Fix lint
    
    * Merge fixs
    
    * Fix JSON
    
    * Added Supplier entries route and initial view file
    
    * Broken progress commit
    
    * Initial work on supplier entries page
    
    * Add some fields
    
    * Add some fields
    
    * Add functionaly to view entries
    Add functionality to add/exit entry
    validate data on adding entry
    
    * Add table sorting.
    Add overdue class (lines are red if supplier is overdue)
    
    * Translation fix
    Smaller sorting icon
    
    * Lint fix
    CSS add-on

---
+++ public/scripts/controllers/supplier_entries.js
@@ -0,0 +1,100 @@
+
+suppliers_app.controller("supplierEntriesController", function ($scope, $http, $filter) {
+    function getAllSuppliers (on_success) {
+        const url = '/suppliers';
+        $http.get(url)
+            .then((res) => {
+                on_success(res);
+            })
+            .catch(err => {
+                sweetAlert('Could not get supplier list: ' + err.message);
+            });
+    }
+
+    function getEntriesByStatus (status, on_success) {
+        const url = '/suppliers/suppliers_gate_info/' + status;
+        $http.get(url)
+            .then((res) => {
+                suplliers_all = res;
+                on_success(res);
+            })
+            .catch(err => {
+                sweetAlert('Could not get entries: ' + err.message);
+            });
+    }
+
+    // Initial order by last entry
+    $scope.orderEntries = 'enterance_time';
+    $scope.isReverseOrder = true;
+
+    // Get suppliers for entry list
+    getAllSuppliers((res) => {
+        $scope.suppliers = res.data.suppliers;
+        setTimeout(() => {
+            innerHeightChange()
+        }, 500)
+    });
+
+    // Get all entries (for 'Inside' status)
+    refreshCurrentEntries();
+
+    // New entry
+    $scope.newEntry = {};
+
+    $scope.addEntry = function () {
+        if (!$scope.suppliers.find(supplier => supplier.supplier_id === $scope.newEntry.supplier_id)) {
+            return sweetAlert(`Unknown supplier, please validate id/vat!`);
+        }
+        if (!$scope.newEntry.supplier_id || !$scope.newEntry.vehicle_plate_number
+            || !$scope.newEntry.number_of_people_entered || !$scope.newEntry.allowed_visa_hours) {
+            return sweetAlert(`Please fill all fields!`);
+        }
+        $http.post(`/suppliers/${$scope.newEntry.supplier_id}/add_gate_record_info/Inside`, $scope.newEntry)
+            .then(done => {
+                sweetAlert(`Supplier entry added!`);
+                $scope.newEntry = {};
+                refreshCurrentEntries();
+            })
+            .catch(err => {
+                sweetAlert(`An error occurred while adding supplier entry ${err.data.data.message}`);
+            });
+    };
+
+    $scope.supplierLeft = function(entry) {
+        $http.post(`/suppliers/${$scope.newEntry.supplier_id}/add_gate_record_info/Outside`, entry)
+            .then(done => {
+                refreshCurrentEntries();
+            })
+            .catch(err => {
+                sweetAlert(`An error occurred while editing supplier entry ${err.data.data.message}`);
+            });
+    };
+
+    // Test if entry is past it's exit time;
+    $scope.checkOverdue = function (entry) {
+        const entryTime = moment(entry.enterance_time);
+        const maxExitTime = entryTime.add(entry.allowed_visa_hours, 'hours');
+        const now = new Date();
+        // If maxExitTime is smaller then now - the supplier is overdue.
+        return maxExitTime.toDate().getTime() < now.getTime();
+    };
+
+    $scope.changeOrderBy = function (orderString) {
+        if ($scope.orderEntries === orderString) {
+            // Only toggle ordering
+            $scope.isReverseOrder = !$scope.isReverseOrder;
+            return;
+        }
+        $scope.orderEntries = orderString;
+    };
+
+    function refreshCurrentEntries() {
+        getEntriesByStatus('Inside', (res) => {
+            $scope.entries = res.data.suppliers;
+            setTimeout(() => {
+                innerHeightChange()
+            }, 500)
+        });
+    }
+
+});
