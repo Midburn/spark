a144a30d9b6450dafa862a0343bcaf0492e61f4f
Nitay Rabinovich <nitayrabi@gmail.com>
2018-04-11 22:01:58 +0300

    Supplier display fixs (#839)
    * Remove redundant data.
    Add date pipe (+ same datepipe in entries)
    Replace dropdown with autocomplete
    
    * Display camp names
    Validate correct camp chosen

--- public/scripts/controllers/supplier_edit.js
+++ public/scripts/controllers/supplier_edit.js
@@ -22,7 +22,7 @@ suppliers_app.controller("supllierShowController", ($scope, $http, $filter) => {
 });
 suppliers_app.controller("supllierEditController", ($scope, $http, $filter) => {
     const supplier_id = document.querySelector('#meta__supplier_id').value;
-    const lang = document.getElementById('meta__lang').value || 'he'; 
+    const lang = document.getElementById('meta__lang').value || 'he';
     angular_getSupplierById($http, $scope, supplier_id)
     if (lang === "he") {
         $scope.status_options = [
@@ -52,27 +52,37 @@ suppliers_app.controller("supllierEditController", ($scope, $http, $filter) => {
     }
     $scope.changeOrderBy = (orderByValue) => {
         $scope.orderMembers = orderByValue;
-    }
+    };
     if (typeof supplier_id !== 'undefined') {
         $scope.current_supplier_id = supplier_id;
         $scope.getCamps();
     }
-    
+
     $scope.addCamp= () => {
         const supplier_id = $scope.current_supplier_id;
-        const camp_id = $scope.add_camp_id;
+        // Convert display name to id (prevent id shown in autocomplete)
+        const add_camp_display_name = $scope.add_camp_display_name;
+        const options = [];
+        document.querySelectorAll("#campList option").forEach(option => options.push(option.value));
+        if (!options.includes(add_camp_display_name)) {
+            // Validate correct camp chosen.
+            sweetAlert("oops", "Unknown camp chosen", "warning");
+            return;
+        }
+        const camp_id = document.querySelector("#campList option[value='"+add_camp_display_name+"']").dataset.id;
         $http.put(`/suppliers/${supplier_id}/camps/${camp_id}`)
              .then(function (res) {
                 // update table with new data
                 $scope.getCamps();
-                $scope.add_camp_id = '';
+                $scope.add_camp_display_name = '';
              }).catch((err) => {
                 if (err.data.data.message.indexOf("Duplicate entry")) {
-                    sweetAlert("!oops","You are trying to add a camp that already exists","warning");
+                    sweetAlert("!oops","You are trying to add a camp that already exists", "warning");
                 }
                 else {
                     sweetAlert("Error!", "Add new camp error: " + err.data.data.message, "error");
                 }
         });
-    }
+    };
+
 }); //end of controller
