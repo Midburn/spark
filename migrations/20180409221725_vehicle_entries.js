4a16d62fc70299a749f34b6fa97bad8354d89101
Yuri Prezument <y@yprez.com>
2018-04-13 21:46:45 +0300

    Fix migration (auto inc. id field)

--- migrations/20180409221725_vehicle_entries.js
+++ migrations/20180409221725_vehicle_entries.js
@@ -4,15 +4,12 @@ exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema
             .createTable('vehicle_entries', table => {
-                table.integer('id').unsigned().notNullable();
+                table.increments('id').primary();
                 table.string('event_id', 15).notNullable();
                 table.datetime('timestamp').notNullable();
                 table.enu('direction', constants.VEHICLE_ENTRY_DIRECTION).notNullable();
-            })
-            .table('vehicle_entries', table => {
                 table.foreign('event_id').references('events.event_id');
-                table.primary('id');
-            })
+            }),
     ]);
 };
 
