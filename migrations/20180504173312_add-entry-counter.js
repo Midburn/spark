ba2eb4f87d645900e384aedc87bc2de289db1277
Nitay Rabinovich <nitayrabi@gmail.com>
2018-05-05 19:20:47 +0300

    Add entry table (#951)
    Add endpoints to handle entries
    Add counter in stats area

--- migrations/20180504173312_add-entry-counter.js
+++ migrations/20180504173312_add-entry-counter.js
@@ -3,11 +3,12 @@ const constants = require('../models/constants.js');
 exports.up = function(knex, Promise) {
     return Promise.all([
         knex.schema
-            .createTable(constants.VEHICLE_ENTRIES_TABLE_NAME, table => {
+            .createTable(constants.ENTRIES_TABLE_NAME, table => {
                 table.increments('id').primary();
                 table.string('event_id', 15).notNullable();
                 table.datetime('timestamp').notNullable();
                 table.enu('direction', constants.ENTRY_DIRECTION).notNullable();
+                table.enu('type', constants.ENTRY_TYPE).notNullable();
                 table.foreign('event_id').references('events.event_id');
             }),
     ]);
