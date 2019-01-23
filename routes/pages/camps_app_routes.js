d1b657d7c59cadef7347dab642f709858572a92c
nitayrabi <nitayrabi@gmail.com>
2019-01-23 21:51:13 +0200

    Fix domain options

--- routes/pages/camps_app_routes.js
+++ routes/pages/camps_app_routes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const userRole = require('../../libs/user_role');
 const passportLib = require('../../libs/passport');
+const constants = require('../../models/constants');
 
 router.get('/', userRole.isLoggedIn(), function (req, res) {
-    const options = {httpOnly: true, overwrite: true, domain: '.midburn.org'};
-    if (process.env.NODE_ENV === 'development') {
-        delete options.domain;
-    }
-    res.cookie('authToken', passportLib.generateJwtToken(req.user.attributes.email, req.user.currentEventId), options);
+    const cookieOptions = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging' ?
+        { overwrite: true, httpOnly: true, domain: constants.MIDBURN_DOMAIN } : { overwrite: true, httpOnly: true };
+    res.cookie('authToken', passportLib.generateJwtToken(req.user.attributes.email, req.user.currentEventId), cookieOptions);
     res.redirect(process.env.COMMUNITIES_URL ?`${process.env.COMMUNITIES_URL}${req.params.lng}/${req.query.path}` : `http://localhost:3006/${req.params.lng}/${req.query.path}`);
 });
 
