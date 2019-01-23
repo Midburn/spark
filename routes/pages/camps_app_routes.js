581b745a8fc75b724ee300f0d25af66b6e76c328
nitayrabi <nitayrabi@gmail.com>
2019-01-23 20:47:11 +0200

    Fix domain

--- routes/pages/camps_app_routes.js
+++ routes/pages/camps_app_routes.js
@@ -4,9 +4,9 @@ const userRole = require('../../libs/user_role');
 const passportLib = require('../../libs/passport');
 
 router.get('/', userRole.isLoggedIn(), function (req, res) {
-    const options = { httpOnly: true, overwrite: true };
-    if (process.env.NODE_ENV === 'production') {
-        options.domain = '.midburn.org';
+    const options = { httpOnly: true, overwrite: true, domain: '.midburn.org'};
+    if (process.env.NODE_ENV === 'development') {
+        delete options.domain;
     }
     res.cookie('authToken', passportLib.generateJwtToken(req.user.attributes.email, req.user.currentEventId), options);
     res.redirect(process.env.COMMUNITIES_URL ?`${process.env.COMMUNITIES_URL}${req.params.lng}/${req.query.path}` : `http://localhost:3006/${req.params.lng}/${req.query.path}`);
