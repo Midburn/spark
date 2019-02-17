a1eebd2b66c8bad65412dcfba8f2a0122ebcf115
Nitay Rabinovich <nitayr@wix.com>
2019-02-18 00:04:32 +0200

    Allow api to get user by (#1021)
    * USe api auth to get user by id
    
    * Add support for multi id search
    
    * Fix lint

--- routes/api-18/routers/users.router.js
+++ routes/api-18/routers/users.router.js
@@ -1,63 +1,80 @@
 const Router = require('express').Router,
-    constants = require('../../../models/constants'),
-    userRole = require('../../../libs/user_role'),
-    usersController = require('../controllers').usersController,
-    helperService = require('../services').helperService;
+  constants = require('../../../models/constants'),
+  userRole = require('../../../libs/user_role'),
+  usersController = require('../controllers').usersController,
+  helperService = require('../services').helperService;
 
 class UsersRouter {
-    constructor() {
-        this.router = Router();
-        this.prefix = constants.ROUTER_PREFIXES.USERS;
-        this.initOpenRoutes();
-        this.initMiddlewares();
-        this.initRoutes();
-        /**
+  constructor () {
+    this.router = Router();
+    this.prefix = constants.ROUTER_PREFIXES.USERS;
+    this.initOpenRoutes();
+    this.initMiddlewares();
+    this.initRoutes();
+    /**
          * Error middleware - catch all `next(Error)` in a single place
          * keep this as the last middleware (and after all routes)
          */
-        this.router.use(helperService.errorMiddleware(this.prefix));
-    }
+    this.router.use(helperService.errorMiddleware(this.prefix));
+  }
 
-    initOpenRoutes() {
-        /**
+  initOpenRoutes () {
+    /**
          * These routes are logged before middleware (e.g loggedIn)
          */
-        /**
+    /**
          * API: (GET) return active user list.
          *  if req.user.isAdmin - return all users
          *  if req.user.isCampManager - return all users from all camps
          *  else return req.user
          * request => /users
          */
-        this.router.route('')
-            .get(usersController.getUsers);
-        /**
+    this.router.route('').get(usersController.getUsers);
+    /**
          * API: (GET) get user by email
          * request => /users/email/:email
          */
-        this.router.route('/email/:email')
-            .get(userRole.isApiLoggedIn(), usersController.getUserByEmail)
+    this.router
+      .route('/email/:email')
+      .get(
+        userRole.isApiLoggedIn(userRole.isLoggedIn()),
+        usersController.getUserByEmail
+      );
+    /**
+         * API: (POST) get user by ids
+         * request => /users/ids
+         */
 
-    }
+  this.router
+          .route('/ids')
+          .post(
+              userRole.isApiLoggedIn(userRole.isAllowedToViewUser()),
+              usersController.getUsersByIds
+          );
+      /**
+       * API: (GET) get user by id
+       * request => /users/:id
+       */
+      this.router
+          .route('/:id')
+          .get(
+              userRole.isApiLoggedIn(userRole.isAllowedToViewUser()),
+              usersController.getUserById
+          );
+  }
 
-    initMiddlewares() {
-        this.router.use(userRole.isLoggedIn());
-    }
-    initRoutes() {
-        /**
+  initMiddlewares () {
+    this.router.use(userRole.isLoggedIn());
+  }
+  initRoutes () {
+    /**
          * Init the different paths for this router.
          */
-        /**
-         * API: (GET) get user by id
-         * request => /users/:id
-         */
-        this.router.route('/:id')
-            .get([userRole.isAllowedToViewUser()], usersController.getUserById);
 
-        this.router.route('/:user_id/join_details')
-            .get(usersController.userJoinDetails);
-
-    }
+    this.router
+      .route('/:user_id/join_details')
+      .get(usersController.userJoinDetails);
+  }
 }
 
 /**
--- routes/pages/camps_app_routes.js
+++ routes/pages/camps_app_routes.js
@@ -9,7 +9,7 @@ router.get('/', userRole.isLoggedIn(), function (req, res) {
         options.domain = '.midburn.org';
     }
     res.cookie('authToken', passportLib.generateJwtToken(req.user.attributes.email, req.user.currentEventId), options);
-    res.redirect(process.env.COMMUNITIES_URL ?`${process.env.COMMUNITIES_URL}${req.params.lng}/${req.query.path}` : `http://localhost:3006/${req.params.lng}/${req.query.path}`);
+    res.redirect(['staging', 'production'].includes(process.env.NODE_ENV) ? `${process.env.COMMUNITIES_URL || 'http://communities:3006'}${req.params.lng}/${req.query.path}` : `http://localhost:3006/${req.params.lng}/${req.query.path}`);
 });
 
 module.exports = router;
