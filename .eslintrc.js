eb820d93a2d4e9d796c198154d99070a51823237
Nitay Rabinovich <nitayr@wix.com>
2019-02-18 19:20:14 +0200

    use first name and last name + drupal data (#1022)
    <!-- PLEASE READ THE FOLLOWING INSTRUCTIONS -->
    
    ### Proposed solution
    <!-- Which specific problem does this PR solve and how?  -->
    <!-- If it fixes a particular Issue, add "Fixes #ISSUE_NUMBER" in your title -->
    
    ### Tradeoffs
    <!-- What are the drawbacks of this solution? Are there alternative ones? -->
    <!-- Think of performance, build time, usability, complexity, coupling…) -->
    
    
    ### Testing Done
    <!-- How have you confirmed this feature works? -->
    
    <!-- BEFORE SUBMITTING YOUR PR, MAKE SURE TO FOLLOW THESE STEPS: -->
    <!-- 1. Pull the latest `master` branch -->
    <!-- 2. Run `npm install` to install all Spark dependencies -->
    <!-- 3. Make sure your code is compliant with the [Spark styleguide](CONTRIBUTING.md) -->
    <!-- 4. If your PR fixes an issue, reference that issue -->
    <!-- 5. If your PR has lots of commits, **rebase** first -->
    <!-- 6. Run `npm test` before submitting your PR -->

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,29 @@
 module.exports = {
-    "extends": "standard",
-    "plugins": [
-        "standard",
-        "promise"
-    ],
-    "rules": {
-        // Used in tests
-        "no-undef": ["off"],
+  extends: 'standard',
+  plugins: ['standard', 'promise'],
+  rules: {
+    // Used in tests
+    'no-undef': ['off'],
 
-        // Style
-        "semi": ["off"],
-        "indent": ["off"],
-        "eol-last": ["off"],
-        "quotes": ["off"],
-        "operator-linebreak": ["off"],
-        "space-before-function-paren": ["off"],
-        "spaced-comment": ["off"],
-        "brace-style": ["off"],
-        "padded-blocks": ["off"],
-        "camelcase": ["off"],
-        "space-infix-ops": ["off"],
-        "comma-spacing": ["off"],
-        "no-trailing-spaces": ["off"],
-        "key-spacing": ["off"],
-        "block-spacing": ["off"],
-        //"space-unary-ops": ["off"],
-        "comma-dangle": ["off"],
-        "one-var": ["off"]
-    }
+    // Style
+    semi: ['off'],
+    indent: ['off'],
+    'eol-last': ['off'],
+    quotes: ['off'],
+    'operator-linebreak': ['off'],
+    'space-before-function-paren': ['off'],
+    'spaced-comment': ['off'],
+    'brace-style': ['off'],
+    'padded-blocks': ['off'],
+    camelcase: ['off'],
+    'space-infix-ops': ['off'],
+    'comma-spacing': ['off'],
+    'no-trailing-spaces': ['off'],
+    'key-spacing': ['off'],
+    'block-spacing': ['off'],
+    //"space-unary-ops": ["off"],
+    'comma-dangle': ['off'],
+    'one-var': ['off'],
+    'func-call-spacing': ['off'],
+  },
 };
