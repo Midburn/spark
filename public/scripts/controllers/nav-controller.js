94411cca740d8f5045eb7930d80e048272e265e6
Nitay Rabinovich <nitayr@wix.com>
2019-03-02 21:18:21 +0200

    Implement communities nav (#1025)
    * Allocations for group manager
    
    * Use events service to check allocation dates.
    
    * Validate user can allocate
    
    * Link to my camp correctly
    
    * Remove console.log

--- public/scripts/controllers/nav-controller.js
+++ public/scripts/controllers/nav-controller.js
@@ -1,6 +1,41 @@
-nav_module.controller ('navController', [
-  'communitiesService',
-  function ($scope, communitiesService) {
-    console.log (communitiesService, $scope);
-  },
+nav_module.controller('navController', [
+    '$scope',
+    'communitiesService',
+    'eventsService',
+    function ($scope, communitiesService, eventsService) {
+        $scope.communitiesService = communitiesService;
+        $scope.eventsService = eventsService;
+
+        /**
+         * Test if allocations (appriciation) is visible.
+         * @returns Boolean
+         */
+        $scope.isAllocationNavVisible = () => {
+            if (!communitiesService || !communitiesService.user) {
+                return false;
+            }
+            return $scope.isCampsAdmin() ||
+                $scope.isArtInstallationsAdmin;
+        };
+
+        $scope.getAllocationGroups = () => {
+            if (!eventsService.isPresaleAvailable()) {
+                return [];
+            }
+            return communitiesService.allocationGroups;
+        };
+
+        $scope.isCampsAdmin = () => {
+            return communitiesService.user && (communitiesService.user.isAdmin || communitiesService.user.isCampsAdmin);
+        };
+
+        $scope.isArtInstallationsAdmin = () => {
+            return communitiesService.user && (communitiesService.user.isAdmin || communitiesService.user.isArtInstallationsAdmin);
+        };
+
+        $scope.getGroupName = (group, lng) => {
+            return communitiesService.getPropertyByLang (group, 'name', lng);
+        };
+
+    },
 ]);
